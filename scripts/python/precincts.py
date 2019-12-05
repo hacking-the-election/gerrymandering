8fd2cc0530965802d3856a8aa4a6df76b9db13e1
lol-cubes <arinmkhare@gmail.com>
2019-12-05 18:58:33 -0500

    write generate_from_file function in precinct.py

--- scripts/python/precincts.py
+++ scripts/python/precincts.py
@@ -1,22 +1,25 @@
-# Precinct-level maps from election-geodata compiled by Nathaniel Kelso and Michal Migurski.
-# Election data from harvard dataverse.
+"""
+Precinct-level maps from election-geodata compiled by Nathaniel Kelso and Michal Migurski.
+Election data from harvard dataverse.
 
-# in election-geodata geojson files, "VTDST10" property is the same as harvard "ed_precinct"
+in election-geodata geojson files, "VTDST10" property is the same as harvard "ed_precinct
+"""
 
 
+import json
 from os import mkdir
-from os.path import dirname, isdir
+from os.path import abspath, dirname, isdir
 import pickle
 
 
-objects_dir = dirname(dirname(__file__)) + '/data/objects'  # where the precinct data is stored
+objects_dir = abspath(dirname(dirname(dirname(__file__)))) + '/data/objects'  # where the precinct data is stored
 
 
 def save(precinct):
     """
     Save the data for a precinct object to a file
     """
-    with open(f'{objects_dir}/{precinct.state}/{precinct.vote_id}', 'w+') as f:
+    with open(f'{objects_dir}/{precinct.state}/{precinct.vote_id}', 'wb+') as f:
         pickle.dump(precinct, f)
 
 
@@ -33,12 +36,25 @@ def area(coords):
     """
     Returns the area of a poylgon with vertices at 2-d list `coords`
     """
-    area = 0
+    a = 0
 
-    for i, j in zip(range(len(coords)), range(-1, len(coords) - 1)):
-        area += (coords[j][0] + coords[i][0]) * (coords[j][1] - coords[i][1])
+    try:
+        for i, j in zip(range(len(coords)), range(-1, len(coords) - 1)):
+            a += (coords[j][0] + coords[i][0]) * (coords[j][1] - coords[i][1])
+    except TypeError as e:
+        return area(coords[0])
 
-    return area / 2
+    return a / 2
+
+
+def convert_to_int(string):
+    """
+    Wrapped error handling for int().
+    """
+    try:
+        return int(string)
+    except Exception:
+        return 0
 
 
 class Precinct:
@@ -75,18 +91,40 @@ class Precinct:
         self.dem_rep_ratio = d_election_sum / r_election_sum
 
         self.dem_average = d_election_sum / len(list(d_election_data.values()))
-        self.rep_average = d_election_sum / len(list(r_elevtion_data.values()))
+        self.rep_average = d_election_sum / len(list(r_election_data.values()))
 
         # create directory for states if it doesn't already exist
         if not isdir(f'{objects_dir}/{self.state}'):
             mkdir(f'{objects_dir}/{self.state}')
 
     @classmethod
-    def generate_from_files(cls, election_data, geojson):
+    def generate_from_files(cls, election_data, geojson, state):
         """
-        Returns a list of precinct objects from the harvard dataverse election data file and the election-geodata file
+        Saves precinct objects from the harvard dataverse election data file and the election-geodata file to files in data/object directory
         """
+        with open(geojson, 'r') as f:
+            data = json.load(f)
+
+        with open(election_data, 'r') as f:
+            content = f.read().strip()
+        
+        data_rows = [row.split('\t') for row in content.split('\n')]
+        data_columns = [[data_rows[x][y] for x in range(len(data_rows))]
+                        for y in range(len(data_rows[0]))]
+        data_dict = {column[0]: column[1:] for column in data_columns}
+        dem_keys = [key for key in data_dict.keys() if key[-2:] == 'dv']
+        rep_keys = [key for key in data_dict.keys() if key[-2:] == 'rv']
+        dem_cols = {data_dict['ed_precinct'][i]: {key: convert_to_int(data_dict[key][i]) for key in dem_keys} for i in range(len(data_dict['ed_precinct']))}
+        rep_cols = {data_dict['ed_precinct'][i]: {key: convert_to_int(data_dict[key][i]) for key in rep_keys} for i in range(len(data_dict['ed_precinct']))}
+
+
+        for precinct in data['features']:
+            precinct_id_geo = f'"{precinct["properties"]["VTDST10"]}"'
+            for i, precinct_id_ele in enumerate(data_dict['ed_precinct']):
+                if precinct_id_ele == precinct_id_geo:
+                    save(Precinct(precinct['geometry']['coordinates'][0], data_dict['precinct'][i], state, precinct_id_geo, dem_cols[precinct_id_ele], rep_cols[precinct_id_ele]))
 
 
 # TODO
-# generate_from_files functions
+# add comments
+# add example file in objects folder
