ce5bfeaa9dd783ec877a81d501be4a63d52051ea
k-vernooy <kai.a.vernooy@gmail.com>
2019-12-15 22:09:05 -0500

    Ignore binary files, move from main

--- cpp/quantify/src/main.cpp
+++ cpp/quantify/src/main.cpp
@@ -1,32 +1,32 @@
-#include "../include/gerry.hpp"
+#include "../include/shape.hpp"
+#include <SDL2/SDL.h>
 
-using namespace rapidjson;
-using namespace std;
+string parse_coordinates(string geoJson) { 
+    // parses a geoJSON state with precincts into a state object
 
-int main(int argc, char* argv[]) {
-
-    ifstream t(argv[1]);
-    stringstream buffer;
-    buffer << t.rdbuf();
-
+    // create json object from geojson string
     Document json;
-    json.Parse(buffer.str().c_str());
+    json.Parse(geoJson.c_str());
 
+    // parse geojson
     Value& s = json["features"][0]["geometry"]["coords"];
     s.SetInt(s.GetInt() + 1);
 
     StringBuffer jsbuffer;
     Writer<StringBuffer> writer(jsbuffer);
     json.Accept(writer);
+    return jsbuffer.GetString();
+}
 
-    cout << jsbuffer.GetString() << endl;
+int main(int argc, char* argv[]) {
 
-
-//     // 2. Modify it by DOM.
-//     Value& s = d["features"];
-//     s.SetInt(s.GetInt() + 1);
-
-// // [features][geometry][coords]
+    if ( argc != 2 ) {
+        cout << "must have 2 arguments: geoJSON file and election geodata";
+    }
+    // ifstream t(argv[1]);
+    // stringstream buffer;
+    // buffer << t.rdbuf();
+    // string geoJSON = buffer.str();
 
     return 0;
 }
