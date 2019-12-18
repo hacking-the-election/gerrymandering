30d94a3143c808556f1a554a273fed449e427bc1
k-vernooy <kai.a.vernooy@gmail.com>
2019-12-17 21:08:58 -0500

    Added shape manipulation functions

--- cpp/quantify/src/shape.cpp
+++ cpp/quantify/src/shape.cpp
@@ -1,16 +1,56 @@
 #include "../include/shape.hpp"
 
-Precinct::Precinct(vector<int> coordinates, int demV, int repV){
-    precinct_border = coordinates;
+vector<vector<int> > expand_border(vector<vector<int> > shape) {
+    vector<vector<int> > expanded;
+    return expanded;
+}
+
+
+double* center(vector<vector<int> > shape) {
+    double coords[2] = { (float) shape[0][0], (float) shape[0][1] };
+    
+    for ( int i = 1; i < shape.size(); i++ ) {
+        coords[0] += shape[i][0];
+        coords[1] += shape[i][1];
+    }
+
+    coords[0] /= shape.size();
+    coords[1] /= shape.size();
+
+    return coords;
+}
+
+double area(vector<vector<int> > shape) {
+    double area = 0;
+    int points = shape.size() - 1;
+
+    for ( int i = 0; i < shape.size(); i++ ) {
+        area += (shape[points][0] + shape[i][0]) * (shape[points][1] - shape[i][1]);
+        points = i;
+    }
+
+    return (area / 2);
+}
+
+Precinct::Precinct(vector<vector<int> > shape, int demV, int repV){
+    precinct_border = shape;
 
     dem = demV;
     rep = repV;
-    
-    dratio = dem / (dem + rep);
 }
 
-State::State(vector<District> dists, vector<Precinct> pres, vector<int> boundary) {
+
+double Precinct::get_ratio() {
+    return dem / (dem + rep);
+}
+
+District::District(vector<vector<int> > shape) {
+    district_border = shape;
+    district_border_expanded = expand_border(district_border);
+}
+
+State::State(vector<District> dists, vector<Precinct> pres, vector<vector<int> > shape) {
     state_districts = dists;
-    state_border = boundary;
+    state_border = shape;
     state_precincts = pres;
 }
