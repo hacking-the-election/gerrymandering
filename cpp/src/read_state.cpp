0140e43c631f2637795dc712fd9105f184608d69
JamesxGamesYT <jamesanxinlian@gmail.com>
2020-01-25 17:10:59 -0500

    fixing stuff

---
+++ cpp/src/read_state.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include "../include/shape.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    string path = string(argv[1]);
+    cout << "Reading state..." << endl;
+    State state = State::read_binary(path);
+    cout << "Read state from " << path << endl;
+    cout << state.to_json() << endl;
+}
