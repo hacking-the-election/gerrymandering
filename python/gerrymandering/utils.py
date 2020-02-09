a42d65aff517c0f93117d980ae53fff253023e10
lol-cubes <arinmkhare@gmail.com>
2020-02-09 11:45:04 -0500

    create python/gerrymandering/utils.py

---
+++ python/gerrymandering/utils.py
@@ -0,0 +1,16 @@
+"""
+Useful computational geometry functions
+"""
+
+
+def border(shapes):
+    """
+    shapes: a list of lists of coords representing the vertices of a
+            polygon. These shapes must be clustered together to make
+            one larger polygon.
+    
+    returns the vertices of the larger polygon that the smaller ones
+    make up
+    """
+
+    pass
