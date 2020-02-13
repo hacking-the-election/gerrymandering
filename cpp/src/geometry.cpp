7434c38f5d92ca18b002d645267b586dee2a0ad0
k-vernooy <kai.a.vernooy@gmail.com>
2020-02-12 19:50:46 -0500

    Add clipper library functionality, remove boost

--- cpp/src/geometry.cpp
+++ cpp/src/geometry.cpp
@@ -12,8 +12,8 @@
 #include "../include/shape.hpp"   // class definitions
 #include "../include/gui.hpp"     // for the draw function
 
-
 #define PI M_PI
+const int c = pow(10, 7);
 
 coordinate Shape::center() {
     // returns the average {x,y} of a shape
@@ -132,16 +132,16 @@ p_index_set get_inner_boundary_precincts(Precinct_Group shape) {
     */
 
     p_index_set boundary_precincts;
-    Shape exterior_border = generate_exterior_border(shape);
+    Multi_Shape exterior_border = generate_exterior_border(shape);
     
     int i = 0;
     
-    for (Precinct p : shape.precincts) {
-        if (are_bordering(p, exterior_border)) {
-            boundary_precincts.push_back(i);
-        }
-        i++;
-    }
+    // for (Precinct p : shape.precincts) {
+    //     if (are_bordering(p, exterior_border)) {
+    //         boundary_precincts.push_back(i);
+    //     }
+    //     i++;
+    // }
 
     return boundary_precincts;
 }
@@ -168,26 +168,26 @@ bool is_colinear(segment s0, segment s1) {
     return (calculate_line(s0) == calculate_line(s1));
 }
 
-Shape generate_exterior_border(Precinct_Group precinct_group) {
+Multi_Shape generate_exterior_border(Precinct_Group precinct_group) {
 
     /*
         Get the exterior border of a shape with interior components.
         Equivalent to 'dissolve' in mapshaper - remove bordering edges
     */
+	Paths subj(precinct_group.precincts.size());
 
-    Shape ext_border; 
+    for (int i = 0; i < precinct_group.precincts.size(); i++)
+        subj[i] = shape_to_clipper_int(precinct_group.precincts[i]);
 
-    boost_multi_polygon border;
+    Paths clip(0);
+    Paths solutions;
 
-    for (Precinct p : precinct_group.precincts) {
-        boost_polygon p_poly = shape_to_poly(p);
-        boost_multi_polygon tmp_poly;
-        union_(border, p_poly, tmp_poly);
-        border = tmp_poly;
-    }
+    Clipper c;
+	c.AddPaths(subj, ptSubject, true);
+	c.AddPaths(clip, ptClip, true);
+	c.Execute(ctUnion, solutions, pftNonZero, pftNonZero);
 
-    ext_border = boost_poly_to_shape(border);
-    return ext_border;
+    return clipper_mult_int_to_shape(solutions);
 }
 
 p_index State::get_addable_precinct(p_index_set available_precincts, p_index current_precinct) {
@@ -195,74 +195,42 @@ p_index State::get_addable_precinct(p_index_set available_precincts, p_index cur
     return ret;
 }
 
-boost_polygon shape_to_poly(Shape shape) {
-       
-    /*
-        Converts a shape object into a boost polygon object
-        by looping over each point and manually adding it to a 
-        boost polygon using assign_points and vectors
-    */
+Path shape_to_clipper_int(Shape shape) {
+    Path p(shape.border.size());
 
-    boost_polygon poly;
-
-    // create vector of boost points
-    vector<boost_point> points;
-    for (coordinate c : shape.border) 
-        points.push_back(boost_point(c[0],c[1])),
-
-    assign_points(poly, shape.border);
-    correct(poly);
-
-    return poly;
-}
-
-Shape boost_poly_to_shape(boost_polygon poly) {
-       
-    /*
-        Convert from a boost polygon into a Shape object.
-        Loop over each point in the polygon, add it to the
-        shape's border.
-    */
-
-    coordinate_set b;
-    vector<boost_point> const& points = poly.outer();
-    
-    for (int i = 0; i < points.size(); ++i) {
-        b.push_back({(float) get<0>(points[i]), (float) get<1>(points[i])});
+    for (coordinate point : shape.border ) {
+        p << IntPoint(point[0] * c, point[1] * c);
     }
 
-    Shape shape(b);
-    return shape;
+    return p;
 }
 
-Shape boost_poly_to_shape(boost_multi_polygon poly) {
-   
-    /*
-        overload the poly_to_shape function to work with multipolygons
-        loop over each polygon, use same process to add to shape's border
+Shape clipper_int_to_shape(Path path) {
+    Shape s;
 
-        !!! WARNING: Should update shape data structure to work with
-            multipolygon set. Not quite sure how to do this yet...
-    */
+    for (IntPoint point : path ) {
+        coordinate p = {(float)((float)point.X / (float)c), (float)((float)point.Y / (float) c)};
+        s.border.push_back(p);
+    }
 
-    coordinate_set b;
+    return s;
+}
 
-    for (boost_polygon p : poly) {
-        vector<boost_point> const& points = p.outer();
+Multi_Shape clipper_mult_int_to_shape(Paths paths) {
+    Multi_Shape ms;
 
-        for (int i = 0; i < points.size(); ++i) {
-            b.push_back({(float) get<0>(points[i]), (float) get<1>(points[i])});
+    for (Path p : paths) {
+        coordinate_set border;
+
+        for (IntPoint point : p) {
+            coordinate p = {(float)((float)point.X / (float)c), (float)((float)point.Y / (float) c)};
+            border.push_back(p);
+            cout << "{" << p[0] << ", " << p[1] << "}" << endl;
         }
+
+        Shape s(border);
+        ms.shapes.push_back(s);
     }
 
-    Shape shape(b);
-    return shape;
-}
-
-void print_shape(Shape shape) {
-    cout << dsv(shape_to_poly(shape)) << endl;
-}
-
-void print_shape(boost_polygon poly) {
-    cout << dsv(poly) << endl;
+    return ms;
 }
