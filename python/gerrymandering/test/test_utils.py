d6326292ecc785fa118fbf80919394621493007e
lol-cubes <arinmkhare@gmail.com>
2020-02-17 16:30:52 -0500

    create single clipping function with union and difference capabilities

--- python/gerrymandering/test/test_utils.py
+++ python/gerrymandering/test/test_utils.py
@@ -16,7 +16,8 @@ sys.path.append(abspath(dirname(dirname(dirname(__file__)))))
 from utils import convert_to_json
 from serialization.load_precincts import load
 from serialization.save_precincts import Precinct
-from gerrymandering.utils import get_equation, get_segments, get_border
+from gerrymandering.utils import (get_equation, get_segments, clip, UNION,
+                                  DIFFERENCE)
 
 
 def print_time(func):
@@ -51,13 +52,13 @@ class TestUtils(unittest.TestCase):
         precincts and time
         """
 
-        timed_get_border = return_time(get_border)
+        timed_get_border = lambda shapes: return_time(clip)(shapes, UNION)
 
-        X = np.arange(1, len(cls.alabama[0]) + 1)
+        X = np.arange(1, len(cls.vermont[0]) + 1)
         Y = np.array([])
-        for i in range(1, len(cls.alabama[0]) + 1):
-            t = timed_get_border([p.coords for p in cls.alabama[0][:i]])
-            np.append(Y, [t])
+        for i in range(1, len(cls.vermont[0]) + 1):
+            t = timed_get_border([p.coords for p in cls.vermont[0][:i]])
+            Y = np.append(Y, [t])
 
         with open("border_time.pickle", "wb+") as f:
             pickle.dump(Y, f)
@@ -92,7 +93,7 @@ class TestUtils(unittest.TestCase):
     @print_time
     def test_get_border(self):
 
-        get_border([p.coords for p in self.vermont[0][:len(self.vermont[0]) // 7]])
+        clip([p.coords for p in self.vermont[0]], UNION)
 
     def test_get_schwartsberg_compactness(self):
         pass
