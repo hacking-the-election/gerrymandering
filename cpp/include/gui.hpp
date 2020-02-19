c0c39b5fbab62ecbb972a388a47e1b5b3471d48b
k-vernooy <kai.a.vernooy@gmail.com>
2020-02-18 23:15:28 -0500

    Update gui functions for polygons with holes so things will compile

--- cpp/include/gui.hpp
+++ cpp/include/gui.hpp
@@ -6,6 +6,6 @@
 const int SCREEN_PIXEL_WIDTH = 640;
 const int SCREEN_PIXEL_HEIGHT = 480;
 
-Uint32* pix_array(coordinate_set shape, int x, int y);
-coordinate_set connect_dots(coordinate_set shape);
+Uint32* pix_array(GeoGerry::coordinate_set shape, int x, int y);
+GeoGerry::coordinate_set connect_dots(GeoGerry::coordinate_set shape);
 void destroy_window(SDL_Window* window);
