05a8e9ccdb327f4b3dc63667730684ee76dbb27f
k-vernooy <kai.a.vernooy@gmail.com>
2020-02-22 12:23:17 -0500

    Debug and reimplement point in poly function

--- cpp/src/geometry.cpp
+++ cpp/src/geometry.cpp
@@ -94,6 +94,9 @@ segments GeoGerry::LinearRing::get_segments() {
     segments segs;
     
     // loop through segments
+    if (border[border.size() - 1] == border[0])
+        border.pop_back();
+
     for (int i = 0; i < border.size(); i++) {
         coordinate c1 = border[i];  // starting coord
         coordinate c2;              // ending coord
@@ -252,28 +255,42 @@ bool point_in_ring(GeoGerry::coordinate coord, GeoGerry::LinearRing lr) {
         quite sure how it works - pulled this one from python
     */
 
-    int intersections = 0;
-    segments segs = lr.get_segments();
+   int cn = 0;
 
-    for (segment s : segs) {
-        if ((s[0] < coord[0] && s[2] < coord[0]) ||
-            (s[0] > coord[0] && s[2] > coord[0]) ||
-            (s[1] < coord[1] && s[3] < coord[1]))
-            continue;
+    // loop through all edges of the polygon
+    for (segment seg : lr.get_segments()) {
 
-        if (s[1] >= coord[1] && s[3] >= coord[1]) {
-            intersections++;
-            continue;
-        }
-        else {
-            vector<double> eq = calculate_line(s);
-            double y_c = eq[0] * coord[0] + eq[1];
-            if (y_c >= coord[1]) intersections++;
+       if (((seg[1] <= coord[1]) && (seg[3] > coord[1])) ||  // an upward crossing
+           ((seg[1] > coord[1]) && (seg[3] <= coord[1]))) {  // a downward crossing
+            double vt = (double)(coord[1]  - seg[1]) / (seg[3] - seg[1]);
+            if (coord[0] < seg[0] + vt * (seg[2] - seg[0])) // coord[0] < intersect
+                ++cn;   // a valid crossing of y = coord[1] right of coord[0]
         }
     }
 
-    std::cout << intersections << std::endl;
-    return (intersections % 2 == 1); // odd intersection
+    return (cn & 1);    // 0 if even (out), and 1 if  odd (in)
+
+    // int intersections = 0;
+    // segments segs = lr.get_segments();
+
+    // for (segment s : segs) {
+    //     if ((s[0] < coord[0] && s[2] < coord[0]) ||
+    //         (s[0] > coord[0] && s[2] > coord[0]) ||
+    //         (s[1] < coord[1] && s[3] < coord[1]))
+    //         continue;
+
+    //     if (s[1] >= coord[1] && s[3] >= coord[1]) {
+    //         intersections++;
+    //         continue;
+    //     }
+    //     else {
+    //         vector<double> eq = calculate_line(s);
+    //         double y_c = eq[0] * coord[0] + eq[1];
+    //         if (y_c >= coord[1]) intersections++;
+    //     }
+    // }
+
+    // return (intersections % 2 == 1); // odd intersection
 }
 
 bool get_inside(GeoGerry::LinearRing s0, GeoGerry::LinearRing s1) {
@@ -298,14 +315,11 @@ bool get_inside_first(GeoGerry::LinearRing s0, GeoGerry::LinearRing s1) {
 }
 
 bool get_inside_d(GeoGerry::LinearRing s0, GeoGerry::LinearRing s1) {
-    int index = 0;
-    for (coordinate c : s0.border) {
-        if (!point_in_ring(c, s1)) {
-            std::cout << c[0] << ", " << c[1] << " failed at index " << index << std::endl;
-            std::cout << s1.to_json() << std::endl;
+    for (int i = 0; i < s0.border.size(); i++) {
+        if (!point_in_ring(s0.border[i], s1)) {
+            std::cout << s0.border[i][0] << ", " << s0.border[i][1] << " failed at index " << i << std::endl;
             return false;
         }
-        index++;
     }
 
     return true;
