ca02fe719434811cf280391fdb71a2550fe5a9cc
k-vernooy <kai.a.vernooy@gmail.com>
2020-02-24 18:35:14 -0500

    Add updated sort function

--- cpp/include/geometry.hpp
+++ cpp/include/geometry.hpp
@@ -45,6 +45,7 @@ double get_median_partisanship(GeoGerry::Precinct_Group pg);
 
 bool point_in_ring(GeoGerry::coordinate coord, GeoGerry::LinearRing lr);
 bool get_inside(GeoGerry::LinearRing s0, GeoGerry::LinearRing s1);
+bool get_inside_or(GeoGerry::LinearRing s0, GeoGerry::LinearRing s1);
 bool get_inside_first(GeoGerry::LinearRing s0, GeoGerry::LinearRing s1);
 bool get_inside_d(GeoGerry::LinearRing s0, GeoGerry::LinearRing s1);
 
