a5126307aa91c5f11a3e34526c1a7d082891cb7c
lol-cubes <arinmkhare@gmail.com>
2020-03-08 21:30:54 -0400

    write code for compactness refinement

--- python/hacking_the_election/communities/compactness_refinement.py
+++ python/hacking_the_election/communities/compactness_refinement.py
@@ -3,9 +3,24 @@ Refines a state broken into communities
 so that they are compact within a threshold.
 """
 
+import math
+import random
+import sys
 
-from hacking_the_election.utils.compactness import get_average_compactness
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+
+from hacking_the_election.utils.compactness import (
+    format_float,
+    get_average_compactness,
+    GIVE_PRECINCTS_COMPACTNESS_KWARGS
+)
 from hacking_the_election.utils.exceptions import LoopBreakException
+from hacking_the_election.utils.geometry import (
+    clip,
+    DIFFERENCE,
+    INTERSECTION
+)
 
 
 def refine_for_compactness(communities, minimum_compactness):
@@ -13,18 +28,58 @@ def refine_for_compactness(communities, minimum_compactness):
     Returns communities that are all below the minimum compactness.
     """
 
+    precincts = {p for c in communities for p in c.precincts.values()}
     for community in communities:
         community.update_compactness()
 
     try:
-        for community in communities:
-            # Find outer circle for community
+        while True:
+            random.shuffle(communities)
+            for community in communities:
+                # Find circle with same area as district.
+                radius = math.sqrt(community.coords.area) / math.pi
+                center = community.coords.centroid.coords[0]
+                circle = Point(*center).buffer(radius)
 
-            # Find precincts that are outside of this circle
+                # Find precincts that need to be added to this community.
+                inside_circle = set()
+                for precinct in (precincts - set(community.precincts.values())):
+                    # Section of precinct that is inside of circle.
+                    circle_intersection = \
+                        clip([circle, precinct.coords], INTERSECTION)
+                    # If precinct and circle are intersecting
+                    if len(circle_intersection.exterior.coords) != 0:
+                        intersection_area = circle_intersection.area
+                        precinct_area = precinct.coords.area
+                        if intersection_area > (precinct_area / 2):
+                            inside_circle.add(precinct)
+                
+                # Find precincts that need to be removed from this community.
+                outside_circle = set()
+                for precinct in community.precincts.values():
+                    # Section of precinct that is outside of circle.
+                    circle_difference = clip([precinct.coords, circle], DIFFERENCE)
+                    # If precinct is not entirely in circle
+                    if len(circle_difference.exterior.coords) != 0:
+                        difference_area = circle_difference.area
+                        precinct_area = precinct.coords.area
+                        if difference_area > (precinct_area / 2):
+                            outside_circle.add(precinct)
 
-            while True:
-                # Add precincts one by one
-                if get_average_compactness(communities) > minimum_compactness:
-                    raise LoopBreakException
+                while True:
+                    # Add precincts one by one
+                    precinct = random.sample(outside_circle)
+                    for other_community in communities:
+                        if precinct in other_community.precincts.values():
+                            other_community.give_precinct(
+                                community,
+                                precinct.vote_id,
+                                **GIVE_PRECINCTS_COMPACTNESS_KWARGS
+                            )
+                            community.update_compactness()
+                            other_community.update_compactness()
+                    if get_average_compactness(communities) > minimum_compactness:
+                        raise LoopBreakException
+
     except LoopBreakException:
         return communities
--- python/hacking_the_election/utils/compactness.py
+++ python/hacking_the_election/utils/compactness.py
@@ -3,6 +3,15 @@ Functions for partisanship refinement step in communities algorithm
 """
 
 
+GIVE_PRECINCTS_COMPACTNESS_KWARGS = {
+    "partisanship": False,
+    "standard_deviation": False,
+    "population": False, 
+    "compactness": True, 
+    "coords": True
+}
+
+
 def get_average_compactness(communities):
     """
     Returns average schwartzberg compactness value of community in
@@ -10,3 +19,26 @@ def get_average_compactness(communities):
     """
     compactness_values = [c.compactness for c in communities]
     return sum(compactness_values) / len(compactness_values)
+
+
+def format_float(n):
+    """
+    Returns float with 3 places to the left and 3 to the right of the
+    decimal point.
+    """
+
+    n_chars = list(str(round(n, 3)))
+
+    # Add zeroes on right side.
+    n_right_digits = len("".join(n_chars).split(".")[-1])
+    if n_right_digits < 3:
+        for i in range(3 - n_right_digits):
+            n_chars.append("0")
+    
+    # Add zeroes on left side.
+    n_left_digits = len("".join(n_chars).split(".")[0])
+    if n_left_digits < 3:
+        for i in range(3 - n_left_digits):
+            n_chars.insert(0, "0")
+
+    return "".join(n_chars)
