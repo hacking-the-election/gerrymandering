c6af1b76587960bce465a5d4965e7893c7c30443
JamesxGamesYT <jamesanxinlian@gmail.com>
2020-03-24 23:21:33 -0400

    saved files for iowa redistrict

---
+++ python/hacking_the_election/utils/graph.py
@@ -0,0 +1,11 @@
+class PrecinctPoint(precinct):
+    def __init__:
+        pass
+    def get_bordering_precincts(precinctpoint):
+        pass
+
+class PrecinctEdge(precinct1, precinct2):
+    def __init__:
+        pass
+    def get_other_precinct():
+        pass
