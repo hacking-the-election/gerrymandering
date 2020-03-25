435ba47dbea73aab82523b5ac8536ead33442c5d
lol-cubes <arinmkhare@gmail.com>
2020-03-25 10:14:44 -0400

    add Precinct class

--- python/hacking_the_election/utils/precinct.py
+++ python/hacking_the_election/utils/precinct.py
@@ -2,3 +2,22 @@
 A class representing the smallest unit of voter
 data in our project's data files: the precinct.
 """
+
+
+class Precinct:
+    """
+    Class containing precinct voter, population, and geodata.
+    Also has a `node` attribute that can be used for graph
+    theory related functions.
+    """
+
+    def __init__(self, rep_data, dem_data, pop, coords, geoid, node):
+
+        self.rep_data = rep_data
+        self.dem_data = dem_data
+        self.pop = pop
+        self.coords = coords
+
+        self.id = geoid
+
+        self.node = node
