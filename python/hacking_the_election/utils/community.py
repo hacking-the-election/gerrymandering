30b0fed5e989366e2f7ed69d6cca2d34687e6ad7
lol-cubes <arinmkhare@gmail.com>
2020-03-25 10:19:48 -0400

    create Community class

--- python/hacking_the_election/utils/community.py
+++ python/hacking_the_election/utils/community.py
@@ -1,3 +1,14 @@
 """
 A class representing political communities
 """
+
+
+class Community:
+    """
+    Political communities - groups of precincts.
+    """
+
+    def __init__(self, precincts, community_id):
+
+        self.precincts = {precinct.id: precinct for precinct in precincts}
+        self.id = community_id
