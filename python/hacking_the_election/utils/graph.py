38cbf3218b4f40fbe04591437a7a0abab70ec949
lol-cubes <arinmkhare@gmail.com>
2020-03-25 10:25:11 -0400

    remove self-defined graph classes

--- python/hacking_the_election/utils/graph.py
+++ python/hacking_the_election/utils/graph.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-class PrecinctPoint(precinct):
-    def __init__:
-        pass
-    def get_bordering_precincts(precinctpoint):
-        pass
-
-class PrecinctEdge(precinct1, precinct2):
-    def __init__:
-        pass
-    def get_other_precinct():
-        pass
-=======
 """
 Graph theory related functions and classes.
 """
->>>>>>> 81464cab4ea9791645c0d256ddf3a7d789ecbce7
