769daf9b594ed37f3c4da5ba86e3a26048109085
lol-cubes <arinmkhare@gmail.com>
2020-03-26 11:55:46 -0400

    create initial configuration file and get sphinx documentation working

---
+++ python/hacking_the_election/communities/initial_configuration.py
@@ -0,0 +1,24 @@
+"""Randomly groups precincts in a state to communities of equal (or as close as
+possible) size.
+"""
+
+
+def create_initial_configuration(precinct_graph, precincts, c_communities):
+    """Randomly groups precincts in a state to communities of equal (or as
+    close as possible) size.
+
+    
+    :param precinct_graph: Graph containing all precincts in state as nodes
+    with precinct ids as attributes.
+    :type precinct_graph: `pygraph.classes.graph.graph`
+
+    :param precincts: List of all precincts in state.
+    :type precincts: list of `hacking_the_election.utils.precinct.Precinct`
+
+    :param n_districts: Number of groups of precincts to return.
+    :type n_districts: `int`
+
+    :return: A list of communities. Each is a contiguous shape with a number of
+    precincts within 1 of that of all the other communities.
+    :rtype: list of `hacking_the_election.utils.community.Community`
+    """
--- python/setup.py
+++ python/setup.py
@@ -10,5 +10,5 @@ setup(
         "Pillow",
         "colormap",
         "easydev",
-        "graph-theory"
+        "python-graph-core"
     ])
