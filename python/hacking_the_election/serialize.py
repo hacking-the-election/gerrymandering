398643063460961d927aca144ecf4a4387dfb367
lol-cubes <arinmkhare@gmail.com>
2020-03-26 10:22:15 -0400

    add election data code in serialize.py

--- python/hacking_the_election/serialize.py
+++ python/hacking_the_election/serialize.py
@@ -23,6 +23,35 @@ from shapely.geometry import Polygon, MultiPolygon
 from hacking_the_election.utils.precinct import Precinct
 
 
+def convert_to_int(string):
+    """
+    Wrapped error handling for int().
+    """
+    try:
+        return int(string)
+    except ValueError:
+        if "." in string:
+            try:
+                return int(string[:string.index(".")])
+            except ValueError:
+                return 0
+        else:
+            return 0
+
+
+def tostring(string):
+    """
+    Removes redundant quotes or converts int to string.
+    """
+    try:
+        if string[0] in ["\"", "'"] and string[-1] in ["\"", "'"]:
+            return string[1:-1]
+        else:
+            return string
+    except TypeError:  # most likely that `string` was of type `int`
+        return str(string)
+
+
 def create_graph(election_file, geo_file, pop_file, state):
     """
     Returns graph with precinct ids stored in nodes (string of XML)
@@ -30,18 +59,68 @@ def create_graph(election_file, geo_file, pop_file, state):
     """
 
     # Read election and geo data files.
-    with open(election_file, "r") as f:
-        election_file_contents = f.read().strip()
-    election_data = [line.split("\t") for line in
-                     election_file_contents.split("\n")]
+    if election_file != "none":
+        with open(election_file, "r") as f:
+            election_file_contents = f.read().strip()
+        election_data = [line.split("\t") for line in
+                         election_file_contents.split("\n")]
+
     with open(geo_file, "r") as f:
         geodata = json.load(f)
 
     with open(f"{dirname(__file__)}/state_metadata.pickle", "r") as f:
         state_metadata = json.load(f)[state]
-    
+    dem_keys = state_metadata[state]["dem_keys"]
+    rep_keys = state_metadata[state]["rep_keys"]
+    json_id  = state_metadata[state]["geo_id"]
+    json_pop = state_metadata[state]["pop_key"]
+    ele_id   = state_metadata[state]["ele_id"]
+
     precincts = []
     precinct_graph = graph()
+
+    # Get election data.
+
+    # {precinct_id: [{dem1:data,dem2:data}, {rep1:data,rep2:data}]}
+    precinct_election_data = {}
+    # If election data in geodata file...
+    if election_file == "none":
+
+        # Fill precinct_election_data
+        for precinct in geodata["features"]:
+            properties = precinct["properties"]
+            precinct_id = "".join(properties[json_id])
+            precinct_election_data[precinct_id] = [
+                {key: convert_to_int(properties[key]) for key in dem_keys},
+                {key: convert_to_int(properties[key]) for key in rep_keys}
+            ]
+    # If there is an election data file...
+    else:
+        election_column_names = election_data[0]
+
+        # Get the index of each of the relevant columns.
+        dem_key_col_indices = \
+            [i for i, col in enumerate(election_column_names)
+             if col in dem_keys]
+
+        rep_key_col_indices = \
+            [i for i, col in enumerate(election_column_names)
+             if col in rep_keys]
+
+        ele_id_col_index = 0
+        for i, col in election_column_names:
+            if col == ele_id:
+                ele_id_col_index = i
+                break
+
+        # Fill precinct_election_data
+        for precinct in election_data[1:]:
+            precinct_election_data[precinct[ele_id_col_index]] = [
+                {election_column_names[i]: precinct[i]
+                 for i in dem_key_col_indices},
+                {election_column_names[i]: precinct[i]
+                 for i in rep_key_col_indices}
+            ]
 
     return write(precinct_graph), precincts
 
@@ -49,7 +128,7 @@ def create_graph(election_file, geo_file, pop_file, state):
 if __name__ == "__main__":
     
     precinct_graph, precincts = create_graph(sys.argv[1:5])
-    
+
     # Save precincts as pickle
     with open(sys.argv[5], "wb+") as f:
         pickle.dump(precincts)
