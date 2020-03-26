016ede432f4c0652714771ee069ad1f07f62619b
lol-cubes <arinmkhare@gmail.com>
2020-03-26 10:36:00 -0400

    add get_if_bordering function

--- python/hacking_the_election/utils/geometry.py
+++ python/hacking_the_election/utils/geometry.py
@@ -3,7 +3,12 @@ Various useful geometric functions.
 """
 
 
-from shapely.geometry import Polygon, MultiPolygon
+from shapely.geometry import (
+    LinearRing,
+    MultiPolygon,
+    MultiLineString,
+    Polygon
+)
 
 
 def _list_to_polygon(polygon_list):
@@ -40,3 +45,41 @@ def geojson_to_shapely(geojson):
         return _list_to_polygon(geojson)
     else:
         raise ValueError("invalid geojson")
+
+
+def get_if_bordering(shape1, shape2, inside=False):
+    """
+    Returns whether or not two shapes (shapely polygons) are bordering as a bool
+    If `inside` is `True`, then the function will assume that `shape2` is
+    inside `shape1`. Therefore order or arguments matters.
+    """
+    if inside:
+        difference = shape1.difference(shape2)
+        try:
+            # The below code illustrated:
+            # ________                       ________
+            # |____   |        ____         |____    |
+            # |____|  | minus |____| equals  ____|   |
+            # |_______|                     |________|
+            # The border of the difference and the subtrahend is this:
+            # ________
+            # |       |
+            #         |
+            # |_______|
+            # It is not a closed figure, so they were bordering.
+
+            # You can see that if they were not bordering, the final
+            # figure in the above illustration would be a closed figure.
+
+            # The final `!= (shape1 == shape2)` is an "exclusive or"
+            return isinstance(
+                LinearRing(difference.exterior.coords).difference(shape2),
+                MultiLineString) != (shape1 == shape2)
+        except AttributeError:
+            return False
+    else:
+        # Doesn't work if one shape is inside the other because it'll
+        # always return false because their intersection would be a
+        # Polygon, but they may still be intersecting.
+        # return isinstance(clip([shape1, shape2], UNION), Polygon)
+        return isinstance(shape1.intersection(shape2), MultiLineString)
