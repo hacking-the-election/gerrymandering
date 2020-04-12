44925499b8c7869aa96fd56f13d7f42b495cd3b8
lol-cubes <arinmkhare@gmail.com>
2020-04-12 19:10:19 -0400

    add test data and write test for get_discontinuous_components function

--- python/hacking_the_election/utils/tests/test_graph.py
+++ python/hacking_the_election/utils/tests/test_graph.py
@@ -1,14 +1,32 @@
+from os.path import dirname
 import unittest
 
-from pygraph.classes.graph import graph
+from pygraph.classes.graph import graph as Graph
+from pygraph.readwrite.markup import read
 
 from hacking_the_election.utils import graph
 
 
+SOURCE_DIR = dirname(__file__)
+
+
 class TestGraph(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+        
+        with open(f"{SOURCE_DIR}/data/random_graph.xml", "r") as f:
+            self.random_graph = read(f.read())
+        
+        self.disconnected_graph = Graph()
+        self.disconnected_graph.add_node(1)
+        self.disconnected_graph.add_node(2)
+
     def test_get_discontinuous_components(self):
-        pass
+        
+        self.assertEqual(graph.get_discontinuous_components(self.random_graph), 1)
+        self.assertEqual(graph.get_discontinuous_components(self.disconnected_graph), 2)
 
     def test_remove_edges_to(self):
         pass
