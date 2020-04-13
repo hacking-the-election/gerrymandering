7e2935a60fa36794a3f41817b581bf9614f18d3c
lol-cubes <arinmkhare@gmail.com>
2020-04-13 18:24:35 -0400

    complete unit test for initial configuration

--- python/hacking_the_election/communities/initial_configuration.py
+++ python/hacking_the_election/communities/initial_configuration.py
@@ -12,7 +12,7 @@ from hacking_the_election.utils.graph import (
 )
 
 
-def _create_community(graph, group_size, group):
+def _create_community(graph, group_size, group, calls):
     """Implements backtracking algorithm for precinct choosing a group of precincts that doesnt make the group of unchosen precincts discontiguous.
 
     :param graph: The graph from which to choose a group of precincts. Should not contain any precincts that belong to another community.
@@ -23,7 +23,11 @@ def _create_community(graph, group_size, group):
 
     :param group: set that precincts should be added to.
     :type group: set of int
+
+    :param calls: a variable that stores the number of times this function was called.
+    :type calls: list
     """
+    calls.append(None)
 
     graph_nodes = graph.nodes()
 
@@ -37,20 +41,20 @@ def _create_community(graph, group_size, group):
         # This is temporary.
         # Just to see if removing this precinct would make an island.
         removed_edges = remove_edges_to(node, graph)
-        if get_discontinuous_components(graph) == len(group) + 1:
+        if get_discontinuous_components(graph) == len(group) + 2:
             # Logic for second condition:
             # Each time a precinct is added to the group,
             # it is completely disconnected from the graph.
             # This means that the number of components should always be
-            # the number of precincts in the group + 1.
+            # the number of precincts in the group + 2.
             eligible_precincts.add(node)
         for edge in removed_edges:
             graph.add_edge(edge)
-    
+
     tried_precincts = set()
     # While not all eligible precincts have been tried.
     while tried_precincts != eligible_precincts:
-        selected_precinct = min(
+        selected_precinct = max(
             eligible_precincts - tried_precincts
         )
         
@@ -58,7 +62,7 @@ def _create_community(graph, group_size, group):
         removed_edges = remove_edges_to(selected_precinct, graph)
         if len(group) == group_size:
             raise CommunityCompleteException
-        _create_community(graph, group_size, group)
+        _create_community(graph, group_size, group, calls)
         # Above call didn't raise error, so the algorithm has
         # backtracked and the value at this step needs to be changed.
 
@@ -68,32 +72,30 @@ def _create_community(graph, group_size, group):
         group.remove(selected_precinct)
         tried_precincts.add(selected_precinct)
 
-    # Backtrack (goes back down ot lower levels of recursion):
+    # Backtrack (goes back down to lower levels of recursion):
     return
 
 
-
-def create_initial_configuration(precinct_graph, precincts, n_communities):
+def create_initial_configuration(precinct_graph, n_communities):
     """Randomly group precincts in a state creating contiguous shapes.
     
     :param precinct_graph: Graph containing all precincts in state as nodes with precinct ids as attributes.
     :type precinct_graph: class:`pygraph.classes.graph.graph`
 
-    :param precincts: List of all precincts in state.
-    :type precincts: List of class:`hacking_the_election.utils.precinct.Precinct`
-
     :param n_communities: Number of groups of precincts to return.
     :type n_communities: `int`
 
-    :return: A list of communities. Each is a contiguous shape with a number of precincts within 1 of that of all the other communities.
-    :rtype: list of class:`hacking_the_election.utils.community.Community`
+    :return: A list of communities. Each is a contiguous shape with a number of precincts within 1 of that of all the other communities. Also the number of times _create_community was called.
+    :rtype: list of class: `hacking_the_election.utils.community.Community` and int
     """
     
+    calls = []
+
     # Nothing having to do with islands is required here,
     # since that is taken care of in the creation of hte graph.
-    s = len(precincts) // n_communities
+    s = len(precinct_graph.nodes()) // n_communities
     community_sizes = [s for _ in range(n_communities)]
-    for i in range(len(precincts) % n_communities):
+    for i in range(len(precinct_graph.nodes()) % n_communities):
         community_sizes[i] += 1
 
     # Sets of node identifiers for each community
@@ -104,7 +106,7 @@ def create_initial_configuration(precinct_graph, precincts, n_communities):
     for community_node_group, community_size in zip(community_node_groups[:-1],
                                                     community_sizes[:-1]):
         try:
-            _create_community(tmp_graph, community_size, community_node_group)
+            _create_community(tmp_graph, community_size, community_node_group, calls)
         except CommunityCompleteException:
             pass
         for node in community_node_group:
@@ -122,4 +124,4 @@ def create_initial_configuration(precinct_graph, precincts, n_communities):
             community.take_precinct(precinct_graph.node_attributes(node)[0])
         communities.append(community)
 
-    return communities
+    return communities, len(calls)
--- python/hacking_the_election/communities/tests/test_initial_configuration.py
+++ python/hacking_the_election/communities/tests/test_initial_configuration.py
@@ -14,31 +14,31 @@ from hacking_the_election.visualization.graph_visualization import \
 class TestInitialConfiguration(unittest.TestCase):
     
     def test_grid_graph(self):
-        """Tests random community generation with a graph of 100 precincts in a grid.
+        """Tests random community generation with a graph of 625 precincts in a grid.
         """
 
         G1 = graph()  # A graph whose nodes are in a order of the grid.
         
         # Add nodes with Precinct object attributes.
-        for x in range(10):
-            for y in range(10):
-                coords = Point(x * 100, y * 100).buffer(25)
+        for x in range(25):
+            for y in range(25):
+                coords = Point(x * 10, y * 10).buffer(2)
                 precinct = Precinct(0, coords, "North Montana",
-                                    str(10*x + y), {"rep": 1}, {"dem": 2})
+                                    str(25*x + y), {"rep": 1}, {"dem": 2})
                 G1.add_node(int(precinct.id), attrs=[precinct])
 
         # Add edges so that degree can be calculated.
         for node in G1.nodes():
-            if node % 10 != 9:
+            if node % 25 != 24:
                 G1.add_edge((node, node + 1))
-            if node // 10 != 9:
-                G1.add_edge((node, node + 10))
+            if node // 25 != 24:
+                G1.add_edge((node, node + 25))
 
         ordered_nodes = sorted(G1.nodes(), key=lambda n: len(G1.node_neighbors[n]))
 
         G2 = graph()  # Graph whose node numbers correspond to their rank by degree.
         for node in G1.nodes():
-            G2.add_node(ordered_nodes.index(node))
+            G2.add_node(ordered_nodes.index(node), attrs=G1.node_attributes(node))
         for node in G1.nodes():
             G2_node = ordered_nodes.index(node)
             for neighbor in G1.node_neighbors[node]:
@@ -46,10 +46,24 @@ class TestInitialConfiguration(unittest.TestCase):
                     G2.add_edge((G2_node, ordered_nodes.index(neighbor)))
                 except AdditionError:
                     pass
-        
 
-        
-        
+        communities, calls = create_initial_configuration(G2, 2)
+        for c, community in enumerate(communities):
+            print(f"COMMUNITY {c}")
+            for precinct in community.precincts.values():
+                print(precinct.id)
+
+        print()
+        print(f"{calls=}")
+
+        def colors(n):
+            if G2.node_attributes(n)[0] in communities[0].precincts.values():
+                return (255, 17, 0)
+            else:
+                return (34, 0, 255)
+
+        visualize_graph(G2, None, lambda n: G2.node_attributes(n)[0].centroid,
+                        colors=colors, sizes=lambda n: 10, show=True)
 
 
 if __name__ == "__main__":
