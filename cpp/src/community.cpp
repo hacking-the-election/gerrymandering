d22a0ac294f67763b099a8048e382066a4ebd740
k-vernooy <kai.a.vernooy@gmail.com>
2020-04-15 11:07:38 -0400

    Add backtracking community algorithm

--- cpp/src/community.cpp
+++ cpp/src/community.cpp
@@ -24,7 +24,7 @@
 
 #include <math.h>    // for rounding functions
 #include <numeric>   // include std::iota
-#include <boost/filesystem.hpp>
+#include <iostream>  // std::cout and std::endl
    
 #include "../include/shape.hpp"       // class definitions
 #include "../include/community.hpp"   // class definitions
@@ -35,26 +35,103 @@
 using namespace std;
 using namespace Geometry;
 using namespace Graphics;
-using namespace boost::filesystem;
 
 
 #define VERBOSE 1
 #define DEBUG 0
 
 
-Communities get_initial_configuration(Graph graph) {
-    
-// Get all the group sizes
+Precinct_Group Community::get_shape() {
+    return Precinct_Group();
+}
 
-// 	Groups = [{} for each community]
-// Tmp_graph = copy of graph (so that the original graph doesn’t lose any edges)
-// For each group size except the last one:
-// _create_community(tmp_graph, group_size, groups[community])
 
-// Do the last community
+void Community::add_node(Node& node) {
+    node_ids.push_back(node.id);
+}
 
-// Create communities out of precinct groups.
 
-// Return ‘em
+void create_community(Geometry::Graph& graph, int group_size, Geometry::Community& group) {
+    vector<Node> eligible_precincts = {}; // all the precincts in the graph that are not already in group and if added will not create an island
+    // How to calculate the latter condition for an eligible precinct:
+    // Remove all the edges connected to that precinct. Check if the number of components of the
+    // graph is more than the number of precincts in the group + 2. 2
 
+    vector<Node> tried_precincts = {};
+
+    while (true) {
+        // This is the islands problem. There are no precincts you can add at this level of 
+        // recursion that don’t eventually lead to the islands problem in higher levels of
+        // recursion, so you have to change a decision you made in lower levels of recursion.
+        
+        if (eligible_precincts == tried_precincts) return;
+
+        vector<Node> not_tried_precincts = {};
+
+        set_difference(eligible_precincts.begin(), 
+            eligible_precincts.end(),
+            tried_precincts.begin(),
+            tried_precincts.end(),
+            inserter(not_tried_precincts, not_tried_precincts.begin())
+        );
+
+        Node selected_precinct = *min_element(not_tried_precincts.begin(), not_tried_precincts.end());
+        // this min() is of the node number (rank by degree.) I'm not sure if we’re doing greatest
+        // degree is 1 or the last one, so this may become max() in the future.
+        
+        group.add_node(selected_precinct);
+        vector<array<int, 2> > removed_edges = graph.remove_edges_to(selected_precinct.id);
+
+        if (group.node_ids.size() == group_size) {
+            // break out of all levels of recursion
+            throw Exceptions::CommunityComplete();
+        }
+        
+        // Adds one precinct every call, but recursion,
+        // so it's actually adding all the precincts every call.
+        create_community(graph, group_size, group); 
+
+        // The above call didn’t raise a CommunityCompleteException, so it has returned from 
+        // all higher levels of recursion back to this call (see red), meaning we have to change 
+        // our decision at this level.
+
+        // undo edge removal
+        for (Edge edge : removed_edges)
+            graph.add_edge(edge);
+
+        group.node_ids.erase(std::remove(group.node_ids.begin(), group.node_ids.end(), selected_precinct.id), group.node_ids.end());
+        tried_precincts.push_back(selected_precinct);
+    }
+
+    return;
+}
+
+
+Communities get_initial_configuration(Graph graph, int n_communities) {
+    Communities communities(n_communities);
+
+    int base = floor((double)graph.vertices.size() / (double)n_communities); // the base num
+    int rem = graph.vertices.size() % n_communities; // how many need to be increased by 1
+
+    vector<int> sizes(n_communities, base);
+    for (int i = sizes.size() - 1; i > sizes.size() - rem - 1; i--) sizes[i]++;
+
+    for (int i = 0; i < sizes.size() - 1; i++) {
+        
+        try {
+            create_community(graph, sizes[i], communities[i]);
+        }
+        catch (Exceptions::CommunityComplete) {
+            cout << "Finished community of size " << sizes[i] << endl;
+        }
+
+        for (int id : communities[i].node_ids) {
+            graph.vertices.erase(id);
+        }
+    }
+
+    for (int i = 0; i < graph.vertices.size(); i++)
+        communities[communities.size() - 1].add_node(graph.vertices[i]);
+
+    return communities;
 }
