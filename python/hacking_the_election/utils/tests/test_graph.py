7e5ece7d4cc94e14b5c2eb8b1a0a722cba7cb1a7
lol-cubes <arinmkhare@gmail.com>
2020-04-19 09:40:48 -0400

    add test for remove_edges_to

--- python/hacking_the_election/utils/tests/test_graph.py
+++ python/hacking_the_election/utils/tests/test_graph.py
@@ -2,10 +2,12 @@ from os.path import dirname
 import unittest
 import time
 
+from shapely.geometry import Point
 from pygraph.classes.graph import graph as Graph
 from pygraph.readwrite.markup import read
 
 from hacking_the_election.utils import graph
+from hacking_the_election.utils.precinct import Precinct
 
 
 SOURCE_DIR = dirname(__file__)
@@ -41,6 +43,13 @@ class TestGraph(unittest.TestCase):
         vermont_removed_edges = graph.remove_edges_to('27', self.vermont_graph)
         print(f"vermont remove_edges_to: {time.time() - start}")
         self.assertEqual(vermont_removed_edges, self.vermont_graph_2)
+
+    def test_get_node_number(self):
+
+        precinct_graph = Graph()
+        precinct = Precinct(0, Point(0, 0).buffer(1), "North Montana", "0", 0, 0)
+        precinct_graph.add_node(0, attrs=[precinct])
+        graph.get_node_number(precinct, precinct_graph)
 
 
 if __name__ == "__main__":
