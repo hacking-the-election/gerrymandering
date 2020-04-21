6e90dab84d74cd7c06c406d7cadd14228f8ad738
k-vernooy <kai.a.vernooy@gmail.com>
2020-04-21 11:42:04 -0400

    Add island linker

--- cpp/script/draw_community.cpp
+++ cpp/script/draw_community.cpp
@@ -22,6 +22,51 @@ using namespace std;
 using namespace Geometry;
 using namespace Graphics;
 
+class NodePair {
+    public:
+        array<int, 2> node_ids;
+        double distance;
+        NodePair() {};
+
+        bool operator<(const NodePair& l2) const {
+            return this->distance < l2.distance;
+        }
+};
+
+
+
+Graph link_islands(Graph graph) {
+    
+    while (graph.get_num_components() > 1) {
+
+        vector<Graph> components = graph.get_components();
+        vector<NodePair> dists;
+
+        for (size_t i = 0; i < components.size(); i++) {
+            for (size_t j = i + 1; j < components.size(); j++) {
+                for (size_t p = 0; p < components[i].vertices.size(); p++) {
+                    for (size_t q = 0; q < components[j].vertices.size(); q++) {
+                        double distance = get_distance((components[i].vertices.begin() + p).value().precinct->get_center(), (components[j].vertices.begin() + q).value().precinct->get_center());
+                        NodePair np;
+                        np.distance = distance;
+                        np.node_ids = {(components[i].vertices.begin() + p).key(), (components[j].vertices.begin() + q).key()};
+                        dists.push_back(np);
+                    }
+                }
+            }
+        }
+
+        array<int, 2> me = min_element(dists.begin(), dists.end())->node_ids;
+        graph.add_edge({me[0], me[1]});
+        
+        // cout << *min_element(distances.begin(), distances.end()) << endl;
+        // get closest two islands
+        // find closest exterior nodes
+    }
+
+    return graph;
+}
+
 
 int main(int argc, char* argv[]) {
     
@@ -45,8 +90,8 @@ int main(int argc, char* argv[]) {
     // auto duration = duration_cast<microseconds>(stop - start); 
     
     // cout << duration.count() << endl; 
-
-    canvas.add_graph(state.network);
+    // link_islands(state.network);
+    canvas.add_graph(link_islands(state.network));
     canvas.draw();
     return 0;
 }
