682da309b7baa2d02b6e1b0953452496fac23b2d
k-vernooy <kai.a.vernooy@gmail.com>
2020-04-23 13:45:15 -0400

    Move island to parse file

--- cpp/script/draw_community.cpp
+++ cpp/script/draw_community.cpp
@@ -22,54 +22,6 @@ using namespace std;
 using namespace Geometry;
 using namespace Graphics;
 
-class NodePair {
-    public:
-        array<int, 2> node_ids;
-        double distance;
-        NodePair() {};
-
-        bool operator<(const NodePair& l2) const {
-            return this->distance < l2.distance;
-        }
-};
-
-
-
-Graph link_islands(Graph graph) {
-    
-    while (graph.get_num_components() > 1) {
-
-        vector<Graph> components = graph.get_components();
-        vector<NodePair> dists;
-
-        for (size_t i = 0; i < components.size(); i++) {
-            for (size_t j = i + 1; j < components.size(); j++) {
-                for (size_t p = 0; p < components[i].vertices.size(); p++) {
-                    for (size_t q = 0; q < components[j].vertices.size(); q++) {
-                        double distance = get_distance((components[i].vertices.begin() + p).value().precinct->get_center(), (components[j].vertices.begin() + q).value().precinct->get_center());
-                        NodePair np;
-                        np.distance = distance;
-                        np.node_ids = {(components[i].vertices.begin() + p).key(), (components[j].vertices.begin() + q).key()};
-                        dists.push_back(np);
-                    }
-                }
-            }
-        }
-
-        array<int, 2> me = min_element(dists.begin(), dists.end())->node_ids;
-        Canvas canvas(500, 500);
-        graph.add_edge({me[0], me[1]});
-        canvas.add_graph(graph);
-        canvas.draw();
-        
-        // cout << *min_element(distances.begin(), distances.end()) << endl;
-        // get closest two islands
-        // find closest exterior nodes
-    }
-
-    return graph;
-}
-
 
 int main(int argc, char* argv[]) {
     
@@ -83,18 +35,8 @@ int main(int argc, char* argv[]) {
 
     // read binary file from path
     State state = State::read_binary(argv[1]);
-    // canvas.add_graph(state.network);
-
-    cout << "drawing" << endl;
-
-    // auto start = high_resolution_clock::now(); 
-    // cout << state.network.get_num_components() << endl;
-    // auto stop = high_resolution_clock::now(); 
-    // auto duration = duration_cast<microseconds>(stop - start); 
-    
-    // cout << duration.count() << endl; 
-    // link_islands(state.network);
-    canvas.add_graph(link_islands(state.network));
+    canvas.add_graph(state.network);
     canvas.draw();
+
     return 0;
 }
