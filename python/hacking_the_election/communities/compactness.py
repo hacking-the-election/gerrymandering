adadf0e0166089d0d1dbaf89e7c645183a22409a
lol-cubes <arinmkhare@gmail.com>
2020-05-16 10:43:37 -0400

    imlpement new compactness process

--- python/hacking_the_election/communities/compactness.py
+++ python/hacking_the_election/communities/compactness.py
@@ -16,7 +16,7 @@ from shapely.geometry import MultiPolygon, Point
 from hacking_the_election.communities.initial_configuration import \
     create_initial_configuration
 from hacking_the_election.utils.exceptions import LoopBreakException
-from hacking_the_election.utils.geometry import get_compactness, get_distance
+from hacking_the_election.utils.geometry import get_distance
 from hacking_the_election.utils.graph import (
     get_components,
     get_giveable_precincts,
@@ -26,7 +26,8 @@ from hacking_the_election.utils.stats import average
 from hacking_the_election.visualization.misc import draw_state
 
 
-N = 40
+M = 20
+N = 20
 
 
 def optimize_compactness(communities, graph, animation_dir=None):
@@ -51,130 +52,111 @@ def optimize_compactness(communities, graph, animation_dir=None):
     best_communities = [copy.copy(c) for c in communities]
     last_communities = []
     iterations_since_best = 0
-    
+
+    n = 0
+
     while True:
 
-        # Find least compact community.
-        community = min(communities, key=lambda c: c.compactness)
-        iteration_compactnesses = [c.compactness for c in communities]
-        rounded_compactnesses = [round(c, 3) for c in iteration_compactnesses]
-        min_compactness = min(iteration_compactnesses)
-        print(rounded_compactnesses, min(rounded_compactnesses))
-
-        # Exit function if algorithm hasn't found new best solution in
-        # the last `N` iterations.
-        if min_compactness < min([c.compactness for c in best_communities]):
-
-            iterations_since_best += 1
-
-            if iterations_since_best >= N:
-
-                # Revert to best community state.
-                for c in best_communities:
-                    for c2 in communities:
-                        if c.id == c2.id:
-                            c2.precincts = c.precincts
-                            c2.update_compactness()
-                for c in communities:
-                    for precinct in c.precincts.values():
-                        precinct.community = c.id
-
-                rounded_compactnesses = [round(c.compactness, 3) for c in communities]
-                print(rounded_compactnesses, min(rounded_compactnesses))
-                if animation_dir is not None:
-                    draw_state(graph, animation_dir)
-                return
-
-        else:
+        # Stop if number of iterations since the best 
+        # communities so far is more than N.
+        if min([c.compactness for c in communities]) \
+                > min([c.compactness for c in best_communities]):
+            # Current communities are new best.
             best_communities = [copy.copy(c) for c in communities]
             iterations_since_best = 0
-        
-        if last_communities != []:
-            # Check if anything changed. If not, exit the function.
+        else:
+            iterations_since_best += 1
+            if iterations_since_best > N:
+                # Revert to best and exit function.
+                for c in communities:
+                    for bc in best_communities:
+                        if c.id == bc.id:
+                            c.precincts = bc.precincts
+                
+                rounded_compactnesses = [round(c.compactness, 3) for c in communities]
+                print(rounded_compactnesses, min(rounded_compactnesses))
+                return
 
-            changed = False
+        # Stop if there has been no change in communities this iteration.
+        if last_communities != []:
             try:
                 for c in communities:
-                    for c2 in last_communities:
-                        if c.id == c2.id:
-                            if c.precincts != c2.precincts:
+                    for lc in last_communities:
+                        if c.id == lc.id:
+                            if c.precincts != lc.precincts:
                                 changed = True
                                 raise LoopBreakException
+                    # Revert to best and exit function.
+                    # (LoopBreakException was not raised,
+                    # so communities did not change).
+                    for c in communities:
+                        for bc in best_communities:
+                            if c.id == bc.id:
+                                c.precincts = bc.precincts
+                
+                    rounded_compactnesses = [round(c.compactness, 3) for c in communities]
+                    print(rounded_compactnesses, min(rounded_compactnesses))
+                    return
             except LoopBreakException:
                 pass
 
-            if not changed:
 
-                # Revert to best community state.
-                for c in best_communities:
-                    for c2 in communities:
-                        if c.id == c2.id:
-                            c2.precincts = c.precincts
-                            c2.update_compactness()
-                for c in communities:
-                    for precinct in c.precincts.values():
-                        precinct.community = c.id
+        community = communities[n]
 
-                rounded_compactnesses = [round(c.compactness, 3) for c in communities]
-                print(rounded_compactnesses, min(rounded_compactnesses))
-                if animation_dir is not None:
-                    draw_state(graph, animation_dir)
-                return
+        # Precinct centroid coords
+        X = []
+        Y = []
+        precinct_areas = []
+        for p in community.precincts.values():
+            X.append(p.centroid[0])
+            Y.append(p.centroid[1])
+            precinct_areas.append(p.coords.area)
+        center = [average(X), average(Y)]
+        radius = math.sqrt(sum(precinct_areas) / math.pi)
 
-        # Not deepcopy so that precinct objects are not copied (saves memory).
-        last_communities = [copy.copy(c) for c in communities]
-        
-        # Get center and radius of equal-area circle.
-        community_precinct_coords = \
-            [p.coords for p in community.precincts.values()]
-        
-        precinct_X = []
-        precinct_Y = []
-        for p in community_precinct_coords:
-            precinct_X.append(list(p.centroid.coords[0])[0])
-            precinct_Y.append(list(p.centroid.coords[0])[1])
-        
-        community_area = sum([p.area for p in community_precinct_coords])
-        # print(community_area)
-        
-        center = [average(precinct_X), average(precinct_Y)]
-        radius = math.sqrt(community_area / math.pi)
-        # print(center, radius)
-        
-        # Give away precincts that are outside circle.
-        giveable_precincts = get_giveable_precincts(graph, communities, community.id)
-        # print([p[0].id for p in giveable_precincts])
-        for precinct, other_community in giveable_precincts:
-            if get_distance(precinct.centroid, center) > radius:
-                community.give_precinct(
-                    other_community, precinct.id, update={"compactness"})
-                if len(get_components(community.induced_subgraph)) > 1:
-                    # Exchange made `community` non-contiguous. Give precinct back
-                    other_community.give_precinct(
-                        community, precinct.id, update={"compactness"})
-                else:
-                    if animation_dir is not None:
-                        draw_state(graph, animation_dir, [Point(*center).buffer(radius)])
-                        # draw_state(graph, animation_dir)
-        
-        # Take precincts inside that are inside circle.
-        takeable_precincts = get_takeable_precincts(graph, communities, community.id)
-        for precinct, other_community in takeable_precincts:
-            if get_distance(precinct.centroid, center) <= radius:
-                other_community.give_precinct(
-                    community, precinct.id, update={"compactness"})
-                if len(get_components(other_community.induced_subgraph)) > 1:
-                    # Exchange made `other_community` non-contiguous. Give precinct back
+        for _ in range(M):
+
+            rounded_compactnesses = [round(c.compactness, 3) for c in communities]
+            print(rounded_compactnesses, min(rounded_compactnesses))
+
+            # Communities that have exchanged precincts with `community`
+            other_communities = set()
+
+            giveable_precincts = get_giveable_precincts(
+                graph, communities, community.id)
+            
+            for precinct, other_community in giveable_precincts:
+                if get_distance(precinct.centroid, center) > radius:
                     community.give_precinct(
-                        other_community, precinct.id, update={"compactness"})
-                else:
-                    if animation_dir is not None:
-                        draw_state(graph, animation_dir, [Point(*center).buffer(radius)])
-                        # draw_state(graph, animation_dir)
+                        other_community, precinct.id)
+                    if len(get_components(community.induced_subgraph)) > 1:
+                        # Giving precinct made `community` non contiguous.
+                        other_community.give_precinct(
+                            community, precinct.id)
+                    else:
+                        other_communities.add(other_community)
+            
+            takeable_precincts = get_takeable_precincts(
+                graph, communities, community.id)
+            
+            for precinct, other_community in takeable_precincts:
+                if get_distance(precinct.centroid, center) <= radius:
+                    other_community.give_precinct(
+                        community, precinct.id)
+                    if len(get_components(other_community.induced_subgraph)) > 1:
+                        # Giving precicnt made `other_community` non contiguous.
+                        community.give_precinct(
+                            other_community, precinct.id)
+                    else:
+                        other_communities.add(other_community)
+                
+            for other_community in other_communities:
+                other_community.update_compactness()
+            community.update_compactness()
 
-        # if animation_dir is not None:
-        #     # draw_state(graph, animation_dir, [Point(*center).buffer(radius)])
-        #     draw_state(graph, animation_dir)
+        n += 1
+        if n == len(communities):
+            n = 0
 
 
 if __name__ == "__main__":
@@ -182,6 +164,7 @@ if __name__ == "__main__":
     with open(sys.argv[1], "rb") as f:
         graph = pickle.load(f)
     communities = create_initial_configuration(graph, int(sys.argv[2]))
+    print("Initial Configuration Generated")
 
     # TO LOAD INIT CONFIG FROM TEXT FILE:
 
@@ -210,6 +193,7 @@ if __name__ == "__main__":
     start_time = time.time()
     optimize_compactness(communities, graph, animation_dir)
     print(f"Compactness optimization took {time.time() - start_time} seconds.")
+    draw_state(graph, None, fpath="test_thing3.png")
 
     with open(sys.argv[4], "wb+") as f:
         pickle.dump(communities, f)
