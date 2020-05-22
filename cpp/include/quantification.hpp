48ce5a0a3ab9c51ab56da1011070985e192778c5
k-vernooy <kai.a.vernooy@gmail.com>
2020-05-22 12:19:54 -0400

    Add header for quantification

---
+++ cpp/include/quantification.hpp
@@ -0,0 +1,21 @@
+/*=======================================
+ quantification.hpp:            k-vernooy
+ last modified:               Fri May 22
+ 
+ Determines how gerrymandered a district
+ is given a community map.
+========================================*/
+
+#pragma once
+#include "shape.hpp"
+#include "geometry.hpp"
+#include "community.hpp"
+
+
+namespace hte {
+    namespace Geometry {
+        std::array<double, 2> get_quantification(Graph&, Communities&, Multi_Polygon&);
+        double get_population(Precinct_Group, Multi_Polygon);
+        double collapse_vals(double a, double b);
+    }
+}
