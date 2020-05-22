c48dd7903c64a81aadf83ac229221e8f63765f83
k-vernooy <kai.a.vernooy@gmail.com>
2020-05-22 12:19:02 -0400

    Add population getter

--- cpp/src/quantification.cpp
+++ cpp/src/quantification.cpp
@@ -6,15 +6,44 @@
  is given a community map
 ========================================*/
 
+#include "../include/quantification.hpp"
 
-#include "../include/shape.hpp"
-#include "../include/community.hpp"
-#include "../include/geometry.hpp"
+double hte::Geometry::collapse_vals(double a, double b) {
+    // where `a` is the republican ratio and b is the general quantification
+    assert((a < 1.0 && a > 0.0));
+    assert((b < 1.0 && b > 0.0));
+    return (b * (2.0 * (0.5 - a)));
+}
 
-using namespace hte::Geometry;
+
+double get_population(Precinct_Group pg, Multi_Polygon mp) {
+    int pop = 0;
+    for (Precinct p : pg.precincts) {
+        // get the overlap between mp and p
+        // create paths array from polygon
+        ClipperLib::Path subj = ring_to_path(p.hull);
+
+        ClipperLib::Paths clip;
+        for (Polygon p : mp.border)
+            clip.push_back(ring_to_path(p.hull));
+
+        ClipperLib::Paths solutions;
+        ClipperLib::Clipper c; // the executor
+
+        // execute union on paths array
+        c.AddPath(subj, ClipperLib::ptSubject, true);
+        c.AddPaths(clip, ClipperLib::ptClip, true);
+        c.Execute(ClipperLib::ctIntersection, solutions, ClipperLib::pftNonZero);
+        Multi_Polygon intersection = paths_to_multi_shape(solutions);
+        double ratio = (intersection.get_area() / p.get_area());
+        pop += ((double)p.pop * ratio);
+    }
+
+    return pop;
+}
 
 
-double get_quantification(Graph& graph, Communities& communities, Multi_Polygon district) {
+std::array<double, 2> Geometry::get_quantification(Graph& graph, Communities& communities, Multi_Polygon district) {
     // determines how gerrymandered `district` is with the `communities` map.
     // communities had better already be updated.
 
@@ -22,6 +51,9 @@ double get_quantification(Graph& graph, Communities& communities, Multi_Polygon 
     bounding_box db = district.get_bounding_box();
 
     // get all communities that overlap the current district
+    // double district_population = 
+    
+
     for (Community& c : communities) {
         if (bound_overlap(c.shape.get_bounding_box(), db)) {
             // get intersection between the two shapes
@@ -47,6 +79,6 @@ double get_quantification(Graph& graph, Communities& communities, Multi_Polygon 
             Multi_Polygon intersection = paths_to_multi_shape(solutions);
         }
     }
-    
-   return val;
+
+   return {val, 0.0};
 }
