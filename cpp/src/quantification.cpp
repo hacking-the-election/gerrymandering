430e3f54e3e734a62e0f38087cbd313c1fc96dae
k-vernooy <kai.a.vernooy@gmail.com>
2020-05-22 23:20:06 -0400

    Fix ratios of partisanships and getting values from polygons

--- cpp/src/quantification.cpp
+++ cpp/src/quantification.cpp
@@ -21,43 +21,71 @@ double hte::Geometry::collapse_vals(double a, double b) {
 }
 
 
-double get_pop(Precinct& pre) {
-    return pre.pop;
-}
-
-double get_dem(Precinct& pre) {
-    return pre.voter_data[POLITICAL_PARTY::DEMOCRAT];
-}
-
-double get_rep(Precinct& pre) {
-    return pre.voter_data[POLITICAL_PARTY::REPUBLICAN];
-}
-
-
-double hte::Geometry::get_attr_from_mask(Precinct_Group pg, Multi_Polygon mp, double (*measure)(Precinct&)) {
+double hte::Geometry::get_population_from_mask(Precinct_Group pg, Multi_Polygon mp) {
     double pop = 0;
+    bounding_box bound = pg.get_bounding_box();
     for (Precinct p : pg.precincts) {
-        // get the overlap between mp and p
-        // create paths array from polygon
-        ClipperLib::Path subj = ring_to_path(p.hull);
+        if (bound_overlap(p.get_bounding_box(), bound)) {
+            // get the overlap between mp and p
+            // create paths array from polygon
+            ClipperLib::Path subj = ring_to_path(p.hull);
 
-        ClipperLib::Paths clip;
-        for (Polygon p : mp.border)
-            clip.push_back(ring_to_path(p.hull));
+            ClipperLib::Paths clip;
+            for (Polygon p : mp.border)
+                clip.push_back(ring_to_path(p.hull));
 
-        ClipperLib::Paths solutions;
-        ClipperLib::Clipper c; // the executor
+            ClipperLib::Paths solutions;
+            ClipperLib::Clipper c; // the executor
 
-        // execute union on paths array
-        c.AddPath(subj, ClipperLib::ptSubject, true);
-        c.AddPaths(clip, ClipperLib::ptClip, true);
-        c.Execute(ClipperLib::ctIntersection, solutions, ClipperLib::pftNonZero);
-        Multi_Polygon intersection = paths_to_multi_shape(solutions);
-        double ratio = (intersection.get_area() / p.get_area());
-        pop += ((double)measure(p) * ratio);
+            // execute union on paths array
+            c.AddPath(subj, ClipperLib::ptSubject, true);
+            c.AddPaths(clip, ClipperLib::ptClip, true);
+            c.Execute(ClipperLib::ctIntersection, solutions, ClipperLib::pftNonZero);
+            Multi_Polygon intersection = paths_to_multi_shape(solutions);
+            double ratio = (intersection.get_area() / p.get_area());
+            pop += ((double)p.pop * ratio);
+        }
     }
 
     return pop;
+}
+
+
+std::map<POLITICAL_PARTY, double> hte::Geometry::get_partisanship_from_mask(Precinct_Group pg, Multi_Polygon mp) {
+    std::map<POLITICAL_PARTY, double> partisanships;
+    bounding_box bound = pg.get_bounding_box();
+
+    for (auto& pair : pg.precincts[0].voter_data) {
+        partisanships[pair.first] = 0;
+    }
+
+    for (Precinct p : pg.precincts) {
+        if (bound_overlap(p.get_bounding_box(), bound)) {
+            // get the overlap between mp and p
+            // create paths array from polygon
+            ClipperLib::Path subj = ring_to_path(p.hull);
+
+            ClipperLib::Paths clip;
+            for (Polygon p : mp.border)
+                clip.push_back(ring_to_path(p.hull));
+
+            ClipperLib::Paths solutions;
+            ClipperLib::Clipper c; // the executor
+
+            // execute union on paths array
+            c.AddPath(subj, ClipperLib::ptSubject, true);
+            c.AddPaths(clip, ClipperLib::ptClip, true);
+            c.Execute(ClipperLib::ctIntersection, solutions, ClipperLib::pftNonZero);
+            Multi_Polygon intersection = paths_to_multi_shape(solutions);
+            double ratio = (intersection.get_area() / p.get_area());
+
+            for (auto& pair : p.voter_data) {
+                partisanships[pair.first] += (pair.second * ratio);
+            }
+        }
+    }
+
+    return partisanships;
 }
 
 LinearRing bound_to_shape(bounding_box box) {
@@ -72,8 +100,8 @@ std::array<double, 2> Geometry::get_quantification(Graph& graph, Communities& co
     bounding_box db = district.get_bounding_box();
     // get all communities that overlap the current district
     Precinct_Group state = Precinct_Group::from_graph(graph);
+    double district_population = get_population_from_mask(state, district);
 
-    double district_population = get_attr_from_mask(state, district, get_pop);
     int largest_index = -1;
     int largest_pop = -1;
 
@@ -81,7 +109,7 @@ std::array<double, 2> Geometry::get_quantification(Graph& graph, Communities& co
         communities[i].update_shape(graph);
         if (bound_overlap(communities[i].shape.get_bounding_box(), db)) {
             // get intersection between the two shapes
-            double pop = get_attr_from_mask(communities[i].shape, district, get_pop);
+            double pop = get_population_from_mask(communities[i].shape, district);
             if (pop > largest_pop) {
                 largest_pop = pop;
                 largest_index = i;
@@ -107,16 +135,14 @@ std::array<double, 2> Geometry::get_quantification(Graph& graph, Communities& co
     c.AddPaths(clip, ClipperLib::ptClip, true);
     c.Execute(ClipperLib::ctDifference, solutions, ClipperLib::pftNonZero);
     Multi_Polygon difference = paths_to_multi_shape(solutions);
-    
-    double dem = get_attr_from_mask(state, difference, get_dem);
-    double rep = get_attr_from_mask(state, difference, get_rep);
+    std::map<POLITICAL_PARTY, double> partisanships = get_partisanship_from_mask(communities[largest_index].shape, difference);
     double partisanship = 0.5;
 
-    if (dem + rep == 0) {
+    if (partisanships[POLITICAL_PARTY::DEMOCRAT] + partisanships[POLITICAL_PARTY::REPUBLICAN] <= 0) {
         std::cout << "if gerry score is not 0 here something has gone horrible wrong i think (or maybe not because there can be precincts outside with 0 dem and 0 rep" << std::endl;
     }
     else {
-        partisanship = rep / (dem + rep);
+        partisanship = partisanships[POLITICAL_PARTY::REPUBLICAN] / (partisanships[POLITICAL_PARTY::DEMOCRAT] + partisanships[POLITICAL_PARTY::REPUBLICAN]);
     }
 
     return {1 - (largest_pop / district_population), partisanship};
