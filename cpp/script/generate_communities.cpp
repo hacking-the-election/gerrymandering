c34d1e9f29ae922f3827ff7e11fe2a7e10e249e5
k-vernooy <kai.a.vernooy@gmail.com>
2020-05-26 09:18:20 -0400

    Update communities output

--- cpp/script/generate_communities.cpp
+++ cpp/script/generate_communities.cpp
@@ -1,6 +1,6 @@
 /*=======================================
  generate_communities.cpp:      k-vernooy
- last modified:                Sat, Feb 8
+ last modified:               Mon, May 25
  
  Run community generation algorithm and 
  print coordinates as geojson for a given
@@ -43,7 +43,7 @@ int main(int argc, char* argv[]) {
     cout << "performing communities algorithm on " << string(argv[1]) << endl;
 
     // read binary file from path
-    Canvas canvas(800, 800);
+    Canvas canvas(900, 900);
     string read_path = string(argv[1]);
     string output_dir = string(argv[2]);
     if (output_dir.substr(output_dir.size() - 1, 1) == "/") {
@@ -65,7 +65,9 @@ int main(int argc, char* argv[]) {
     
     // get initial random configuration
     Communities init_config = karger_stein(state.network, n_communities);
-    cout << "got initial random configuration, saving to file..." << endl;
+    for (int i = 0; i < init_config.size(); i++) {
+        init_config[i].update_shape(state.network);
+    }
     save(init_config, output_dir + "/init_config_shape.txt");
 
     cout << "optimizing for political communities..." << endl;
@@ -76,13 +78,20 @@ int main(int argc, char* argv[]) {
 
     cout << "optimizing for better districts..." << endl;
     // get districts, save to district output
-    Communities districts = get_communities(state.network, communities, 0.01, output_dir, false);
-    save(districts, output_dir + "/redistricts_shape.txt");
+    Communities redistricts = get_communities(state.network, communities, 0.01, output_dir, false);
+    save(redistricts, output_dir + "/redistricts_shape.txt");
     canvas.clear();
-    canvas.add_outlines(to_outline(districts));
+    canvas.add_outlines(to_outline(redistricts));
     canvas.save_image(ImageFmt::BMP, output_dir + "/img/redistricts");
 
     cout << "quantifying current districts..." << endl;
+    string abs_line;
+    string coll_line;
+    string part_line;
+
+    Canvas absolute_q(900, 900);
+    Canvas collapsed_q(900, 900);
+
     for (int i = 0; i < state.districts.size(); i++) {
         map<POLITICAL_PARTY, double> quantification = get_quantification(state.network, communities, state.districts[i]);
         double partisanship = 0.5;
@@ -91,16 +100,91 @@ int main(int argc, char* argv[]) {
         double collapsed = collapse_vals(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], partisanship);
 
         // save quantification values to file
+        part_line += "[";
+        for (auto& pair : quantification) {
+            if (pair.first == POLITICAL_PARTY::DEMOCRAT) part_line += "\"DEM\":";
+            else if (pair.first == POLITICAL_PARTY::REPUBLICAN) part_line += "\"REP\":";
+            else if (pair.first == POLITICAL_PARTY::GREEN) part_line += "\"GRE\":";
+            else if (pair.first == POLITICAL_PARTY::INDEPENDENT) part_line += "\"IND\":";
+            else if (pair.first == POLITICAL_PARTY::LIBERTARIAN) part_line += "\"LIB\":";
+            else if (pair.first == POLITICAL_PARTY::REFORM) part_line += "\"REF\":";
+            else if (pair.first == POLITICAL_PARTY::OTHER) part_line += "\"OTH\":";
+            else break;
+            part_line += to_string(pair.second);
+            part_line += ",";
+        }
+    
+        part_line.pop_back();
+        part_line += "]";
 
+        abs_line += to_string(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION]);
+        coll_line += to_string(collapsed);
+        
+        if (i != state.districts.size() - 1) {
+            part_line += "\t";
+            coll_line += "\t";
+            abs_line += "\t"; 
+        }
+
+        // save quantification values to file
         // create visualizations of the output
-        canvas.clear();
-        canvas.add_outlines(to_outline(state.districts[i], quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], true));
-        canvas.save_image(ImageFmt::BMP, output_dir + "/img/current_abs_quant");
-        canvas.clear();
-        canvas.add_outlines(to_outline(state.districts[i], collapsed, false));
-        canvas.save_image(ImageFmt::BMP, output_dir + "/img/current_0_1");
+        absolute_q.add_outlines(to_outline(state.districts[i], quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], true));
+        collapsed_q.add_outlines(to_outline(state.districts[i], collapsed, false));
     }
 
+    absolute_q.save_image(ImageFmt::BMP, output_dir + "/img/current_abs_quant");
+    collapsed_q.save_image(ImageFmt::BMP, output_dir + "/img/current_0_1");
     cout << "quantifying redistricted districts..." << endl;
+    writef(abs_line + "\n" + coll_line + "\n" + part_line + "\n", output_dir + "/stats/districts/quantification.tsv");
+    absolute_q.clear();
+    collapsed_q.clear();
+    abs_line.clear();
+    coll_line.clear();
+    part_line.clear();
+
+    for (int i = 0; i < redistricts.size(); i++) {
+        Multi_Polygon ext = generate_exterior_border(redistricts[i].shape);
+        map<POLITICAL_PARTY, double> quantification = get_quantification(state.network, communities, ext);
+        double partisanship = 0.5;
+        if (quantification[POLITICAL_PARTY::DEMOCRAT] + quantification[POLITICAL_PARTY::REPUBLICAN] != 0)
+            partisanship = quantification[POLITICAL_PARTY::REPUBLICAN] / (quantification[POLITICAL_PARTY::DEMOCRAT] + quantification[POLITICAL_PARTY::REPUBLICAN]);
+        double collapsed = collapse_vals(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], partisanship);
+
+        // save quantification values to file
+        part_line += "[";
+        for (auto& pair : quantification) {
+            if (pair.first == POLITICAL_PARTY::DEMOCRAT) part_line += "\"DEM\":";
+            else if (pair.first == POLITICAL_PARTY::REPUBLICAN) part_line += "\"REP\":";
+            else if (pair.first == POLITICAL_PARTY::GREEN) part_line += "\"GRE\":";
+            else if (pair.first == POLITICAL_PARTY::INDEPENDENT) part_line += "\"IND\":";
+            else if (pair.first == POLITICAL_PARTY::LIBERTARIAN) part_line += "\"LIB\":";
+            else if (pair.first == POLITICAL_PARTY::REFORM) part_line += "\"REF\":";
+            else if (pair.first == POLITICAL_PARTY::OTHER) part_line += "\"OTH\":";
+            else break;
+            part_line += to_string(pair.second);
+            part_line += ",";
+        }
+
+        part_line.pop_back();
+        part_line += "]";
+
+        abs_line += to_string(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION]);
+        coll_line += to_string(collapsed);
+
+        if (i != state.districts.size() - 1) {
+            part_line += "\t";
+            coll_line += "\t";
+            abs_line += "\t"; 
+        }
+
+        // create visualizations of the output
+        absolute_q.add_outlines(to_outline(ext, quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], true));
+        collapsed_q.add_outlines(to_outline(ext, collapsed, false));
+    }
+
+    writef(abs_line + "\n" + coll_line + "\n" + part_line + "\n", output_dir + "/stats/redistricts/quantification.tsv");
+    absolute_q.save_image(ImageFmt::BMP, output_dir + "/img/redistricts_abs_quant");
+    collapsed_q.save_image(ImageFmt::BMP, output_dir + "/img/redistricts_0_1");
+
     return 0;
 }
