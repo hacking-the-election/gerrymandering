634eaf5e6b3fa250657de95b23bbfb582a1f6024
k-vernooy <kai.a.vernooy@gmail.com>
2020-06-18 14:12:12 -0400

    Move util functions into separate namespace, add hte header

---
+++ cpp/include/hte.h
@@ -0,0 +1,18 @@
+
+/*========================================
+ hte.hpp:                        k-vernooy
+ last modifi ed:               Wed, Jun 17
+
+ Main include header for the HtE library.
+=========================================*/
+
+#ifndef HTE_H
+#define HTE_H
+
+#include "geometry.h"
+#include "algorithm.h"
+#include "data.h"
+#include "graphics.h"
+#include "graphics.h"
+
+#endif
