5bae632a28d9dc3ffa8658a68c393275538c0686
k-vernooy <kai.a.vernooy@gmail.com>
2020-07-28 21:23:36 -0400

    Update scripts; get code to compileable state

--- cpp/script/generate_communities.cpp
+++ cpp/script/generate_communities.cpp
@@ -1,6 +1,6 @@
 /*=======================================
  generate_communities.cpp:      k-vernooy
- last modified:                Thu, Jun 18
+ last modified:                  Mon, Jun 22
  
  Run community generation algorithm and 
  print coordinates as geojson for a given
@@ -11,182 +11,31 @@
 #include <iostream>
 #include <chrono>
 
-#include "../include/util.hpp"
-#include "../include/shape.hpp"
-#include "../include/geometry.hpp"
-#include "../include/canvas.hpp"
-#include "../include/community.hpp"
-#include "../include/quantification.hpp"
-#include <boost/filesystem.hpp>
+#include "../include/hte.h"
 
 using namespace boost::filesystem;
 using namespace std;
-using namespace hte::Geometry;
-using namespace hte::Graphics;
+using namespace hte;
 
-#define MAX_TIME 40000
 
+double GetCompactnessToMinimize(Communities& c) {
+    return (Average(c, GetPreciseCompactness));
+}
 
 int main(int argc, char* argv[]) {
-    
-    /*
-        A driver program for the community algorithm
-        See community.cpp for the implementation of the algorithm
-    */
-
-    if (argc != 3) {
-        cerr << "generate_communities: usage: <state.dat> <output_dir>" << endl;
-        return 1;
+    State state = State::fromFile(argv[1]);
+    Communities cs = KargerStein(state.network, stoi(argv[2]));
+    for (int i = 0; i < cs.size(); i++) {
+        cs[i].resetShape(state.network);
     }
 
-    srand(time(NULL));
-    cout << "performing communities algorithm on " << string(argv[1]) << endl;
+    Canvas c(900, 900);
+    c.addOutlines(ToOutline(cs));
+    c.drawToWindow();
+    c.clear();
 
-    // read binary file from path
-    Canvas canvas(900, 900);
-    string read_path = string(argv[1]);
-    string output_dir = string(argv[2]);
-    if (output_dir.substr(output_dir.size() - 1, 1) == "/") {
-        output_dir = output_dir.substr(0, output_dir.size() - 1);
-    }
-
-    create_directory(path(output_dir));
-    create_directory(path(output_dir + "/anim"));
-    create_directory(path(output_dir + "/anim/redistricts"));
-    create_directory(path(output_dir + "/anim/communities"));
-    create_directory(path(output_dir + "/img"));
-    create_directory(path(output_dir + "/stats"));
-    create_directory(path(output_dir + "/stats/districts"));
-    create_directory(path(output_dir + "/stats/redistricts"));
-    create_directory(path(output_dir + "/stats/communities"));
-
-    State state = State::from_binary(read_path);
-    int n_communities = state.districts.size();
-    
-    // get initial random configuration
-    // Communities init_config = load("../output/louisiana/init_config_shape.txt", state.network);
-    Communities init_config = karger_stein(state.network, n_communities);
-    for (int i = 0; i < init_config.size(); i++) {
-        init_config[i].update_shape(state.network);
-    }
-    save(init_config, output_dir + "/init_config_shape.txt");
-
-    cout << "optimizing for political communities..." << endl;
-    Communities communities = get_communities(state.network, init_config, 0.1, output_dir, true);
-    save(communities, output_dir + "/communities_shape.txt");
-    canvas.add_outlines(to_outline(communities));
-    canvas.save_image(ImageFmt::BMP, output_dir + "/img/communities");
-
-    cout << "quantifying current districts..." << endl;
-    string abs_line;
-    string coll_line;
-    string part_line;
-
-    Canvas absolute_q(900, 900);
-    Canvas collapsed_q(900, 900);
-
-    for (int i = 0; i < state.districts.size(); i++) {
-        map<POLITICAL_PARTY, double> quantification = get_quantification(state.network, communities, state.districts[i]);
-        double partisanship = quantification[POLITICAL_PARTY::REPUBLICAN];
-        //if (quantification[POLITICAL_PARTY::DEMOCRAT] + quantification[POLITICAL_PARTY::REPUBLICAN] != 0)
-            //partisanship = quantification[POLITICAL_PARTY::REPUBLICAN] / (quantification[POLITICAL_PARTY::DEMOCRAT] + quantification[POLITICAL_PARTY::REPUBLICAN]);
-        double collapsed = collapse_vals(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], partisanship);
-
-        // save quantification values to file
-        part_line += "[";
-        for (auto& pair : quantification) {
-            if (pair.first == POLITICAL_PARTY::DEMOCRAT) part_line += "\"DEM\":";
-            else if (pair.first == POLITICAL_PARTY::REPUBLICAN) part_line += "\"REP\":";
-            else if (pair.first == POLITICAL_PARTY::GREEN) part_line += "\"GRE\":";
-            else if (pair.first == POLITICAL_PARTY::INDEPENDENT) part_line += "\"IND\":";
-            else if (pair.first == POLITICAL_PARTY::LIBERTARIAN) part_line += "\"LIB\":";
-            else if (pair.first == POLITICAL_PARTY::REFORM) part_line += "\"REF\":";
-            else if (pair.first == POLITICAL_PARTY::OTHER) part_line += "\"OTH\":";
-            else break;
-            part_line += to_string(pair.second);
-            part_line += ",";
-        }
-    
-        part_line.pop_back();
-        part_line += "]";
-
-        abs_line += to_string(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION]);
-        coll_line += to_string(collapsed);
-        
-        if (i != state.districts.size() - 1) {
-            part_line += "\t";
-            coll_line += "\t";
-            abs_line += "\t"; 
-        }
-
-        // save quantification values to file
-        // create visualizations of the output
-        absolute_q.add_outline_group(to_outline(state.districts[i], quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], true));
-        collapsed_q.add_outline_group(to_outline(state.districts[i], collapsed, false));
-    }
-
-    absolute_q.save_image(ImageFmt::BMP, output_dir + "/img/current_abs_quant");
-    collapsed_q.save_image(ImageFmt::BMP, output_dir + "/img/current_0_1");
-    writef(abs_line + "\n" + coll_line + "\n" + part_line + "\n", output_dir + "/stats/districts/quantification.tsv");
-
-    // get districts, save to district output
-    cout << "optimizing for better districts..." << endl;
-    Communities redistricts = get_communities(state.network, communities, 0.01, output_dir, false);
-    save(redistricts, output_dir + "/redistricts_shape.txt");
-    canvas.clear();
-    canvas.add_outlines(to_outline(redistricts));
-    canvas.save_image(ImageFmt::BMP, output_dir + "/img/redistricts");
-
-    cout << "quantifying redistricted districts..." << endl;
-    absolute_q.clear();
-    collapsed_q.clear();
-    abs_line.clear();
-    coll_line.clear();
-    part_line.clear();
-
-    for (int i = 0; i < redistricts.size(); i++) {
-        MultiPolygon ext = generate_exterior_border(redistricts[i].shape);
-        map<POLITICAL_PARTY, double> quantification = get_quantification(state.network, communities, ext);
-        double partisanship = 0.5;
-        if (quantification[POLITICAL_PARTY::DEMOCRAT] + quantification[POLITICAL_PARTY::REPUBLICAN] != 0)
-            partisanship = quantification[POLITICAL_PARTY::REPUBLICAN] / (quantification[POLITICAL_PARTY::DEMOCRAT] + quantification[POLITICAL_PARTY::REPUBLICAN]);
-        double collapsed = collapse_vals(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], partisanship);
-
-        // save quantification values to file
-        part_line += "[";
-        for (auto& pair : quantification) {
-            if (pair.first == POLITICAL_PARTY::DEMOCRAT) part_line += "\"DEM\":";
-            else if (pair.first == POLITICAL_PARTY::REPUBLICAN) part_line += "\"REP\":";
-            else if (pair.first == POLITICAL_PARTY::GREEN) part_line += "\"GRE\":";
-            else if (pair.first == POLITICAL_PARTY::INDEPENDENT) part_line += "\"IND\":";
-            else if (pair.first == POLITICAL_PARTY::LIBERTARIAN) part_line += "\"LIB\":";
-            else if (pair.first == POLITICAL_PARTY::REFORM) part_line += "\"REF\":";
-            else if (pair.first == POLITICAL_PARTY::OTHER) part_line += "\"OTH\":";
-            else break;
-            part_line += to_string(pair.second);
-            part_line += ",";
-        }
-
-        part_line.pop_back();
-        part_line += "]";
-
-        abs_line += to_string(quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION]);
-        coll_line += to_string(collapsed);
-
-        if (i != state.districts.size() - 1) {
-            part_line += "\t";
-            coll_line += "\t";
-            abs_line += "\t"; 
-        }
-
-        // create visualizations of the output
-        absolute_q.add_outline_group(to_outline(ext, quantification[POLITICAL_PARTY::ABSOLUTE_QUANTIFICATION], true));
-        collapsed_q.add_outline_group(to_outline(ext, collapsed, false));
-    }
-
-    writef(abs_line + "\n" + coll_line + "\n" + part_line + "\n", output_dir + "/stats/redistricts/quantification.tsv");
-    absolute_q.save_image(ImageFmt::BMP, output_dir + "/img/redistricts_abs_quant");
-    collapsed_q.save_image(ImageFmt::BMP, output_dir + "/img/redistricts_0_1");
-
+    GradientDescentOptimization(state.network, cs, GetCompactnessToMinimize);
+    c.addOutlines(ToOutline(cs));
+    c.drawToWindow();
     return 0;
 }
--- cpp/script/serialize_state.cpp
+++ cpp/script/serialize_state.cpp
@@ -1,6 +1,6 @@
 /*=======================================
  serialize_state.cpp:           k-vernooy
- last modified:               Sun, May 17
+ last modified:               Sun, Jun 21
  
  Given precinct and district geodata, and
  precinct voter data, writes to a binary 
@@ -14,7 +14,7 @@
 #include "../include/hte.h"
 
 using namespace std;
-using namespace hte::Geometry;
+using namespace hte;
 
 
 int main(int argc, char* argv[]) {
@@ -29,8 +29,8 @@ int main(int argc, char* argv[]) {
 
     vector<string> new_argv{};
 
-    map<ID_TYPE, string> ids;
-    map<POLITICAL_PARTY, string> voter_heads;
+    map<IdType, string> ids;
+    map<PoliticalParty, string> voter_heads;
 
 
     for (int i = 0; i < argc; i++) {
@@ -53,18 +53,17 @@ int main(int argc, char* argv[]) {
                 string val = rem.substr(rem.find(":") + 1, rem.find(search) - rem.find(":") - 1);
                 val = string(val.begin() + 1, val.end() - 1);
 
-                // i'm so sorry about this if statement
-                if (key == "GEO") ids[ID_TYPE::GEOID] = val;
-                else if (key == "ELE") ids[ID_TYPE::ELECTIONID] = val;
-                else if (key == "POP") ids[ID_TYPE::POPUID] = val;
-                else if (key == "DEM") voter_heads[POLITICAL_PARTY::DEMOCRAT] = val;
-                else if (key == "REP") voter_heads[POLITICAL_PARTY::REPUBLICAN] = val;
-                else if (key == "LIB") voter_heads[POLITICAL_PARTY::LIBERTARIAN] = val;
-                else if (key == "REF") voter_heads[POLITICAL_PARTY::REFORM] = val;
-                else if (key == "GRE") voter_heads[POLITICAL_PARTY::GREEN] = val;
-                else if (key == "IND") voter_heads[POLITICAL_PARTY::INDEPENDENT] = val;                
-                else if (key == "OTH") voter_heads[POLITICAL_PARTY::OTHER] = val;
-                else if (key == "TOT") voter_heads[POLITICAL_PARTY::TOTAL] = val;
+                if (key == "GEO") ids[IdType::GEOID] = val;
+                else if (key == "ELE") ids[IdType::ELECTIONID] = val;
+                else if (key == "POP") ids[IdType::POPUID] = val;
+                else if (key == "DEM") voter_heads[PoliticalParty::Democrat] = val;
+                else if (key == "REP") voter_heads[PoliticalParty::Republican] = val;
+                else if (key == "LIB") voter_heads[PoliticalParty::Libertarian] = val;
+                else if (key == "REF") voter_heads[PoliticalParty::Reform] = val;
+                else if (key == "GRE") voter_heads[PoliticalParty::Green] = val;
+                else if (key == "IND") voter_heads[PoliticalParty::Independent] = val;                
+                else if (key == "OTH") voter_heads[PoliticalParty::Other] = val;
+                else if (key == "TOT") voter_heads[PoliticalParty::Total] = val;
                 else cout << "unrecognized key " << key << endl;
 
                 if (!done) arg = rem.substr(rem.find(","), rem.size() - rem.find(","));
@@ -83,54 +82,48 @@ int main(int argc, char* argv[]) {
     // generate state from files
     if (argc == 5) {
         // read files into strings
-        string precinct_geoJSON = readf(new_argv[1]);
-        string voter_data = readf(new_argv[2]);
-        string district_geoJSON = readf(new_argv[3]);
+        string precinct_geoJSON = ReadFile(new_argv[1]);
+        string voter_data = ReadFile(new_argv[2]);
+        string district_geoJSON = ReadFile(new_argv[3]);
         write_path = string(new_argv[4]);
-        state = State::generate_from_file(precinct_geoJSON, voter_data, district_geoJSON, voter_heads, ids);
+        state = State::GenerateFromFile(precinct_geoJSON, voter_data, district_geoJSON, voter_heads, ids);
     }
     else {
         // read files into strings
-        string precinct_geoJSON = readf(new_argv[1]);
-        string district_geoJSON = readf(new_argv[2]);
+        string precinct_geoJSON = ReadFile(new_argv[1]);
+        string district_geoJSON = ReadFile(new_argv[2]);
         write_path = string(new_argv[3]);
-        state = State::generate_from_file(precinct_geoJSON, district_geoJSON, voter_heads, ids);
+        state = State::GenerateFromFile(precinct_geoJSON, district_geoJSON, voter_heads, ids);
     }
 
-    state.to_binary(write_path);  // write as binary
+    state.toFile(write_path);
     
     cout << "precincts:\t" << state.precincts.size() << endl;
     cout << "districts:\t" << state.districts.size() << endl;
 
-    map<POLITICAL_PARTY, int> total_voter_data;
+    map<PoliticalParty, int> total_voter_data;
     int total_popu = 0;
-    for (auto& par : state.precincts[0].voter_data) {
+    for (auto& par : state.precincts[0].voterData) {
         total_voter_data[par.first] = 0;
     }
 
     for (Precinct pre : state.precincts) {
-        for (auto& par : pre.voter_data) {
+        for (auto& par : pre.voterData) {
             total_voter_data[par.first] += par.second;
-        }
-
-        if (pre.shape_id == "560051901_s1") {
-            for (auto& par : pre.voter_data) {
-                cout << par.second << endl;
-            }
         }
 
         total_popu += pre.pop;
     }
 
     for (auto& par : total_voter_data) {
-        if (par.first == POLITICAL_PARTY::DEMOCRAT) cout << "dem:\t";
-        else if (par.first == POLITICAL_PARTY::REPUBLICAN) cout << "rep:\t";
-        else if (par.first == POLITICAL_PARTY::TOTAL) cout << "total:\t";
-        else if (par.first == POLITICAL_PARTY::LIBERTARIAN) cout << "lib:\t";
-        else if (par.first == POLITICAL_PARTY::GREEN) cout << "green:\t";
-        else if (par.first == POLITICAL_PARTY::INDEPENDENT) cout << "ind:\t";
-        else if (par.first == POLITICAL_PARTY::REFORM) cout << "reform:\t";
-        else if (par.first == POLITICAL_PARTY::OTHER) cout << "other:\t";
+        if (par.first == PoliticalParty::Democrat) cout << "dem:\t";
+        else if (par.first == PoliticalParty::Republican) cout << "rep:\t";
+        else if (par.first == PoliticalParty::Total) cout << "total:\t";
+        else if (par.first == PoliticalParty::Libertarian) cout << "lib:\t";
+        else if (par.first == PoliticalParty::Green) cout << "green:\t";
+        else if (par.first == PoliticalParty::Independent) cout << "ind:\t";
+        else if (par.first == PoliticalParty::Reform) cout << "reform:\t";
+        else if (par.first == PoliticalParty::Other) cout << "other:\t";
 
         cout << par.second << endl;
     }
