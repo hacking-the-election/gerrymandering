8a9ed11e43fd15f6d96e8386fe491d0a2d4b7acf
lol-cubes <arinmkhare@gmail.com>
2020-07-29 23:19:21 -0400

    implement gradient ascent

--- python/hacking_the_election/communities/__main__.py
+++ python/hacking_the_election/communities/__main__.py
@@ -14,12 +14,20 @@ import sys
 
 from pygraph.classes.graph import graph as Graph
 
-from hacking_the_election.utils.community import Community
+from hacking_the_election.utils.community import (
+    Community,
+    create_initial_configuration
+)
+from hacking_the_election.utils.stats import average
 from hacking_the_election.utils.graph import (
     contract,
+    get_articulation_points,
     light_copy
 )
 from hacking_the_election.visualization.misc import draw_state
+
+
+UPDATE_ATTRIBUTES = {"partisanship_stdev", "imprecise_compactness", "population"}
 
 
 def _get_score(communities):
@@ -32,7 +40,7 @@ def _get_score(communities):
     :rtype: float
     """
     
-    return 0.0
+    return average([c.imprecise_compactness for c in communities])
 
 def _get_all_exchanges(precinct_graph, communities):
     """Finds all the possible ways that a community map can be changed via a single precinct exchanges between two communities.
@@ -43,43 +51,33 @@ def _get_all_exchanges(precinct_graph, communities):
     :param communities: A list of the communities that divy up the precincts in precinct_graph
     :type communities: list of `hacking_the_election.utils.community.Community`
 
-    :return: A dictionary mapping precincts to lists of communities it can be given to.
-    :rtype: dict mapping `hacking_the_election.utils.precinct.Precinct` to list of `hacking_the_election.utils.community.Community`
+    :return: A set of tuples containing a precinct and a community it can be sent to.
+    :rtype: set of tuple of `hacking_the_election.utils.precinct.Precinct` and int
     """
 
-    return {}
+    exchanges = set()
 
+    community_dict = {c.id: c for c in communities}
+    community_articulation_points = {}
+    for community in communities:
+        community_articulation_points[community.id] = \
+            get_articulation_points(community.induced_subgraph)
 
-def _create_initial_configuration(precinct_graph, n_communities):
-    """Creates a list of communities based off of a state precinct-map represented by a graph.
+    for node in precinct_graph.nodes():
 
-    Implementation of Karger-Stein algorithm, except modified a bit to
-    make the partitions of similar sizes.
+        precinct = precinct_graph.node_attributes(node)[0]
+        node_community = precinct.community
 
-    :param precinct_graph: A graph with each node representing a precinct, with precincts stored as node attributes.
-    :type precinct_graph: `pygraph.classes.graph.graph`
-    """
+        if node in community_articulation_points[node_community]:
+            continue
 
-    # Create copy of `precinct_graph` without precinct data.
-    G = light_copy(precinct_graph)
-
-    while len(G.nodes()) > n_communities:
-        attr_lengths = {}  # Links edges to the number of nodes they contain.
-        edges = set(G.edges())
-        for i in range(min(100, len(edges))):
-            e = edges.pop()
-            attr_lengths[e] = (len(G.node_attributes(e[0]))
-                             + len(G.node_attributes(e[1])))
-        contract(G, min(attr_lengths))
-
-    # Create community objects from nodes.
-    communities = [Community(i, precinct_graph) for i in range(n_communities)]
-    for i, node in enumerate(G.nodes()):
-        for precinct_node in G.node_attributes(node):
-            communities[i].take_precinct(
-                precinct_graph.node_attributes(precinct_node)[0])
-
-    return communities
+        for neighbor in precinct_graph.neighbors(node):
+            neighbor_community = \
+                precinct_graph.node_attributes(neighbor)[0].community
+            if neighbor_community != node_community:
+                exchanges.add((precinct, neighbor_community))
+    
+    return exchanges
 
 
 def generate_communities_gradient_ascent(precinct_graph, n_communities, animation_dir=None):
@@ -105,6 +103,37 @@ def generate_communities_gradient_ascent(precinct_graph, n_communities, animatio
             pass
 
     communities = create_initial_configuration(precinct_graph, n_communities)
+    for community in communities:
+        for attribute in UPDATE_ATTRIBUTES:
+            eval(f"community.update_{attribute}()")
+    community_dict = {community.id: community for community in communities}
+
+    while True:
+        best_exchange = None
+        can_be_better = False
+        largest_score = _get_score(communities)
+        exchanges = _get_all_exchanges(precinct_graph, communities)
+
+        for precinct, community_id in exchanges:
+            init_community = community_dict[precinct.community]
+            other_community = community_dict[community_id]
+            if init_community.give_precinct(
+                    other_community, precinct.id, UPDATE_ATTRIBUTES):
+
+                score = _get_score(communities)
+                if score > largest_score:
+                    largest_score = score
+                    best_exchange = (init_community, other_community, precinct.id)
+                    can_be_better = True
+                other_community.give_precinct(
+                    init_community, precinct.id, UPDATE_ATTRIBUTES)
+        
+        if not can_be_better:
+            break
+
+        print(largest_score)
+        best_exchange[0].give_precinct(
+            best_exchange[1], best_exchange[2], UPDATE_ATTRIBUTES)
 
     return communities
 
@@ -150,6 +179,8 @@ if __name__ == "__main__":
         communities = generate_communities_simulated_annealing(*args)
     elif sys.argv[4] == "GA":
         communities = generate_communities_gradient_ascent(*args)
+
+    # draw_state(precinct_graph, None, fpath="test_gradient_ascent.png")
 
     # Write to file.
     with open(sys.argv[3], "wb+") as f:
