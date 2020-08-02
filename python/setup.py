f2f9625845c115d8001e3086900a96b535cd9227
lol-cubes <arinmkhare@gmail.com>
2020-08-02 15:49:59 -0400

    update setup.py to work without needing to pre-install cython

--- python/setup.py
+++ python/setup.py
@@ -1,18 +1,28 @@
 from setuptools import setup, find_packages
 
-from Cython.Build import cythonize
-import numpy as np
 
-setup(
-    name='hacking_the_election',
-    packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "matplotlib",
-        "shapely",
-        "Pillow",
-        "python-graph-core",
-        "cython"
-    ],
-    ext_modules=cythonize("hacking_the_election/utils/*.pyx"),
-    include_dirs=[np.get_include()])
+if sys.argv[1:3] == ['build_ext', '--inplace']:
+    # Compile and install cython code.
+
+    import numpy as np
+    from Cython.Build import cythonize
+
+    setup(
+        ext_modules=cythonize('hacking_the_election/utils/*.pyx'),
+        include_dirs=[np.get_include]
+    )
+
+else:
+    setup(
+        name='hacking_the_election',
+        packages=find_packages(),
+        install_requires=[
+            "numpy",
+            "matplotlib",
+            "shapely",
+            "Pillow",
+            "python-graph-core"
+            "networkx",
+            "cython"
+        ]
+    )
