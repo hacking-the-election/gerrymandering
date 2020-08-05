5660edb22b318afe14405e9122e589cc79434820
lol-cubes <arinmkhare@gmail.com>
2020-08-05 13:59:52 -0400

    clean up and speed up algorithm implementations

--- python/hacking_the_election/communities/__main__.py
+++ python/hacking_the_election/communities/__main__.py
@@ -1,21 +1,11 @@
 """
-Implementation of the simulated annealing and gradient ascent for
+Implementation of the simulated annealing and greedy algorithms for
 generating political communities.
 
 Usage:
-python3 -m hacking_the_election.communities <serialized_state_graph_path> <n_communities> <output_path> <algorithm>
+python3 -m hacking_the_election.communities <serialized_state_graph_path> <n_communities> <output_path> <algorithm> [image_path]
 
-<algorithm> can either be "SA" or "GA"
-grid search:
-    - We just have to run gradient ascent a bunch of times.
-    - for simulated annealing we must change:
-        - starting temp.
-        - cooling function.
-        - number of epochs.
-        - probability function.
-    - For simulated annealing, not only do we have to consider how much
-        it optimizes the score, but we also must consider how long it takes.
-        Will that just be multiplying the two values, together?
+<algorithm> can either be "SA" for simulated annealing or "GA" for greedy algorithm.
 """
 
 import pickle
@@ -35,7 +25,7 @@ UPDATE_ATTRIBUTES = {"population", "partisanship_stdev"}
 
 T_MAX = 30
 EPOCHS = 40000
-COOL = 0.99976
+ALPHA = 0.99976  # Cooling factor
 
 VERBOSE = True
 
@@ -54,18 +44,32 @@ def _get_score(communities):
     # Population compactness and geometric compactness.
     compactness_score = 0
     for community in communities:
+        community_compactness_score = 0
+
+        # Calculate all populations and all distances,
+        # Save the max distance.
         precinct_populations = {}
-        max_population = 0
-        for precinct in community.precincts.values():
-            precinct_populations[precinct.id] = precinct.pop
-            if precinct.pop > max_population:
-                max_population = precinct.pop
+        precinct_distances = {}
+        max_distance = 0
 
         for precinct in community.precincts.values():
-            distance = get_distance(
+            precinct_populations[precinct.id] = precinct.pop
+            
+            precinct_distances[precinct.id] = get_distance(
                 precinct.centroid, community.centroid)
-            compactness_score += (distance
-                * (precinct_populations[precinct.id] / max_population))
+            if precinct_distances[precinct.id] > max_distance:
+                max_distance = precinct_distances[precinct.id]
+
+        # Use distances to calculate compactness score for this community.
+        for precinct in community.precincts.values():
+            community_compactness_score += (
+                (precinct_distances[precinct.id] / max_distance)
+              * (precinct_populations[precinct.id] / community.population)
+            )
+        
+        compactness_score += community_compactness_score
+
+    compactness_score /= len(communities)
     
     # Population distribution.
     state_pop = sum([c.population for c in communities])
@@ -74,7 +78,7 @@ def _get_score(communities):
         - sum([abs(c.population - ideal_pop) for c in communities]) / state_pop)
 
     # Partisanship stdev.
-    partisanship_score = average([c.partisanship_stdev for c in communities])
+    partisanship_score = 1 - average([c.partisanship_stdev for c in communities])
 
     return average([compactness_score, population_score, partisanship_score])
 
@@ -85,7 +89,7 @@ def _get_all_exchanges(precinct_graph, communities):
     :param precinct_graph: A graph with each node representing a precinct, with precincts stored as node attributes.
     :type precinct_graph: `networkx.Graph`
 
-    :param communities: A list of the communities that divy up the precincts in precinct_graph
+    :param communities: A list of the communities that divvy up the precincts in precinct_graph
     :type communities: list of `hacking_the_election.utils.community.Community`
 
     :return: A set of tuples containing a precinct and a community it can be sent to.
@@ -119,17 +123,64 @@ def _get_all_exchanges(precinct_graph, communities):
 def _cool(t):
     """Temperature cooling function.
     """
-    return t * COOL
+    return t * ALPHA
 
 
 def _probability(temp, current_energy, new_energy):
-    """The probability of doing a given exchange.
+    """The probability of doing an exchange.
     """
-    return temp / T_MAX
+    if new_energy > current_energy:
+        # Always do exchanges that are good.
+        return 1
+    else:
+        return temp / T_MAX
 
 
-def generate_communities_gradient_ascent(precinct_graph, n_communities, animation_dir=None):
-    """Generates a set of political communities using gradient ascent.
+def _get_random_exchange(precinct_graph, communities, articulation_points):
+    """Finds a random precinct and a community it can be sent to without:
+        - Making any communities distcontguous.
+        - Creating any 0-precinct communities
+
+    :param precinct_graph: A graph with each node representing a precinct, with precincts stored as node attributes.
+    :type precinct_graph: `networkx.Graph`
+
+    :param communities: A list of the communities that divvy up the precincts in precinct_graph
+    :type communities: list of `hacking_the_election.utils.community.Community`
+
+    :param articulation_points: The articulation points. Provide them if you already know them and don't want them to be recalculated.
+    :type articulation points: dict mapping int to set, or NoneType
+
+    :return: A tuple containing a precinct, the community it can be sent to, and a dict of articulation points by community.
+    :rtype: tuple of `hacking_the_election.utils.precinct.Precinct`, int, and dict mapping int to set of int.
+    """
+
+    community_dict = {c.id: c for c in communities}
+
+    if articulation_points is None:
+        articulation_points = {}
+        for c in communities:
+            articulation_points[c.id] = \
+                set(nx.articulation_points(c.induced_subgraph))
+
+    nodes = precinct_graph.nodes
+    for node in random.sample(nodes, len(nodes)):
+        precinct = precinct_graph.nodes[node]['precinct']
+
+        articulation_point = precinct.node in articulation_points[precinct.community]
+        empty_community = len(community_dict[precinct.community].precincts) == 1
+        if articulation_point or empty_community:
+            continue
+
+        for neighbor in precinct_graph.neighbors(node):
+            neighbor_community = \
+                precinct_graph.nodes[neighbor]['precinct'].community
+            if neighbor_community != precinct.community:
+                return precinct, neighbor_community, articulation_points
+
+# ------- algorithm implementation functions ------- #
+
+def generate_communities_greedy(precinct_graph, n_communities, animation_dir=None):
+    """Generates a set of political communities using the greedy algorithm.
 
     :param precinct_graph: A graph with each node representing a precinct, with precincts stored as node attributes.
     :type precinct_graph: `networkx.Graph`
@@ -150,41 +201,57 @@ def generate_communities_gradient_ascent(precinct_graph, n_communities, animatio
         except FileExistsError:
             pass
 
+    # Create initial configuration and update attributes.
     communities = create_initial_configuration(precinct_graph, n_communities)
     for community in communities:
         for attribute in UPDATE_ATTRIBUTES:
             eval(f"community.update_{attribute}()")
     community_dict = {community.id: community for community in communities}
 
+    i = 0
+
+    if VERBOSE:
+        print("Score      \tIteration")
+
     while True:
+        i += 1
         best_exchange = None
-        can_be_better = False
         largest_score = _get_score(communities)
         exchanges, _ = _get_all_exchanges(precinct_graph, communities)
 
         for precinct, community_id in exchanges:
+            # Communities involved in exchange.
             init_community = community_dict[precinct.community]
             other_community = community_dict[community_id]
-            if init_community.give_precinct(
-                    other_community, precinct.id, UPDATE_ATTRIBUTES):
 
-                score = _get_score(communities)
-                if score > largest_score:
-                    largest_score = score
-                    best_exchange = (init_community, other_community, precinct.id)
-                    can_be_better = True
-                other_community.give_precinct(
-                    init_community, precinct.id, UPDATE_ATTRIBUTES)
-        
-        if not can_be_better:
-            break
+            # Give the precinct and recalculate the score.
+            init_community.give_precinct(
+                other_community, precinct.id, UPDATE_ATTRIBUTES)
+            score = _get_score(communities)
+            if score > largest_score:
+                # Mark this as the best exchange if it gives a score
+                # higher than the best so far.
+                largest_score = score
+                best_exchange = (init_community, other_community, precinct.id)
+            # Undo the exchange to try different ones.
+            other_community.give_precinct(
+                init_community, precinct.id, UPDATE_ATTRIBUTES)
 
         if VERBOSE:
-            print(largest_score)
+            sys.stdout.write(f"\r" + "".join([" " for _ in range(20)]))
+            sys.stdout.write(f"\r{round(largest_score, 8)}\t{i}")
+            sys.stdout.flush()
+
+        if best_exchange is None:
+            # We have reached local maximum. There are no exchanges
+            # that can immediately improve from this point.
+            if VERBOSE:
+                print()
+            return communities
+
+        # perform best exchange.
         best_exchange[0].give_precinct(
             best_exchange[1], best_exchange[2], UPDATE_ATTRIBUTES)
-
-    return communities
 
 
 def generate_communities_simulated_annealing(precinct_graph, n_communities,
@@ -217,46 +284,47 @@ def generate_communities_simulated_annealing(precinct_graph, n_communities,
             pass
 
     communities = create_initial_configuration(precinct_graph, n_communities)
-    community_dict = {c.id: c for c in communities}
+    for community in communities:
+        for attribute in UPDATE_ATTRIBUTES:
+            eval(f"community.update_{attribute}()")
+    community_dict = {community.id: community for community in communities}
 
     current_energy = _get_score(communities)
     temp = T_MAX
+    articulation_points = None
+
+    if VERBOSE:
+        print("Score      \tTemperature  \tEpoch")
 
     for epoch in range(EPOCHS):
-        
-        exchanges, _ = _get_all_exchanges(precinct_graph, communities)
-        choice = random.sample(exchanges, 1)[0]
-        init_community = None
-        other_community = None
 
-        # Choose a random exchange.
-        while True:
-            choice = random.choice(
-                [c for c in exchanges if c is not choice])
-            
-            init_community = community_dict[choice[0].community]
-            other_community = community_dict[choice[1]]
+        # Choose random exchange.
+        precinct, other_community_id, articulation_points = \
+            _get_random_exchange(precinct_graph, communities, articulation_points)
+        init_community = community_dict[precinct.community]
+        other_community = community_dict[other_community_id]
 
-            if init_community.give_precinct(other_community, choice[0].id):
-                break
-        
+        # Perform chosen exchange.
+        init_community.give_precinct(
+            other_community, precinct.id, update=UPDATE_ATTRIBUTES)
+        # Calculate the new energy after this exchange.
         new_energy = _get_score(communities)
 
-        if new_energy > current_energy:
-            # Go ahead with exchange because it increases energy.
+        # Go ahead with exchange with certain probability.
+        if (prob(temp, current_energy, new_energy) > random.random()):
             current_energy = new_energy
-        
-        elif (prob(temp, current_energy, new_energy) > random.random()):
-            # Go ahead with exchange because of probability function.
-            current_energy = new_energy
-        
+            last_epoch_change = True
+            # Reset articulation points because they need to be recalculated.
+            articulation_points = None
         else:
-            # Don't go ahead with exchange.
-            other_community.give_precinct(init_community, choice[0].id)
+            # Exchange was not chosen by probability function.
+            other_community.give_precinct(
+                init_community, precinct.id, update=UPDATE_ATTRIBUTES)
+            last_epoch_change = False
 
         if VERBOSE:
             sys.stdout.write(f"\r" + "".join([" " for _ in range(20)]))
-            sys.stdout.write(f"\r{round(current_energy, 8)}\t{round(temp, 8)}\t{epoch}")
+            sys.stdout.write(f"\r{round(current_energy, 8)}\t{round(temp, 8)}\t{epoch + 1}")
             sys.stdout.flush()
 
         temp = cool(temp)
@@ -280,7 +348,7 @@ if __name__ == "__main__":
         GA_times = []
         for _ in range(10):
             start_time = time.time()
-            generate_communities_gradient_ascent(*args)
+            generate_communities_greedy(*args)
             GA_times.append(time.time() - start_time)
         # SA_times = []
         # for _ in range(100):
@@ -295,10 +363,11 @@ if __name__ == "__main__":
             communities = generate_communities_simulated_annealing(
                 *args, prob=_probability, cool=_cool)
         elif sys.argv[4] == "GA":
-            communities = generate_communities_gradient_ascent(*args)
+            communities = generate_communities_greedy(*args)
         print(f"RUN TIME: {time.time() - start_time}")
 
-        draw_state(precinct_graph, None, fpath="test_algorithm.png")
+        if len(sys.argv) > 5:
+            draw_state(precinct_graph, None, fpath=sys.argv[5])
 
         # Write to file.
         with open(sys.argv[3], "wb+") as f:
