7ea7ba80699cc1fce9d6cbaf014ed739720aabe4
k-vernooy <kai.a.vernooy@gmail.com>
2021-08-03 23:56:56 -0400

    Add recursive vector print method

--- cpp/include/util.h
+++ cpp/include/util.h
@@ -1,3 +1,6 @@
+#ifndef _HTE_UTIL_H
+#define _HTE_UTIL_H
+
 #include <exception>
 #include <string>
 #include <vector>
@@ -18,5 +21,57 @@ std::string ReadFile(std::string path);
 int RandInt(int start, int end);
 double RandUnitInterval();
 
-template<class T>
+template<typename T>
 double GetStdev(std::vector<T>& data);
+
+template<typename T>
+void PrintVec(const T& el)
+{
+    std::cout << el;
+};
+
+template<typename T>
+void PrintVec(const hte::LinearRing<T>& vec)
+{
+    std::cout << "{";
+    for (size_t i = 0; i < vec.size() - 1; i++)
+    {
+        PrintVec(vec[i]);
+        std::cout << ", ";
+    }
+
+    PrintVec(vec.back());
+    std::cout << "}";
+};
+
+template<typename T>
+void PrintVec(const hte::Polygon<T>& vec)
+{
+    std::cout << "{";
+    for (size_t i = 0; i < vec.size() - 1; i++)
+    {
+        PrintVec(vec[i]);
+        std::cout << ", ";
+    }
+
+    PrintVec(vec.back());
+    std::cout << "}";
+};
+
+template<typename T>
+void PrintVec(const std::vector<T>& vec)
+{
+    std::cout << "{";
+    for (size_t i = 0; i < vec.size() - 1; i++)
+    {
+        PrintVec(vec[i]);
+        std::cout << ", ";
+    }
+
+    PrintVec(vec.back());
+    std::cout << "}";
+    std::cout.flush();
+};
+
+
+#endif
